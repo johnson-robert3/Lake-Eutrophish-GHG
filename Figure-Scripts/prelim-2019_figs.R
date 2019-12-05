5b7ab4699e3c23e677bee9e61cf58bbc5aa0fe20
Robert Johnson <johnson.robert.333@gmail.com>
2019-12-05 14:38:02 -0600

    initial commit

---
+++ Figure-Scripts/prelim-2019_figs.R
@@ -0,0 +1,83 @@
+# Preliminary figures to view Hort Farm pond data
+
+
+# select the Hort Farm data
+farm_data = lake_conc %>%
+   filter(location=="ISU Hort Farm") %>%
+   # add factor columns nutrient and fish additions
+   mutate(add_nuts = c("control", "control", "control", "+ nuts", "+ nuts", "+ nuts"),
+          add_fish = c("none", "high", "low", "none", "low", "high"))
+
+
+# Methane
+
+# regular
+
+png(file="Farm_data_CH4_prelim.png")
+# windows()
+
+ggplot(farm_data) +
+   geom_col(aes(x = factor(add_nuts),
+                y = ch4_lake,
+                fill = add_fish),
+            position="dodge2") +
+   scale_x_discrete(limits = c("control", "+ nuts"),
+                    labels = c("Control", "+ Nutrients")) +
+   scale_fill_manual(limits = c("none", "low", "high"),
+                     values = c("gray60", "seagreen3", "seagreen4"),
+                     name = "Fish density") +
+   xlab("") +
+   ylab("Methane (uM)") +
+   theme_classic() +
+   theme(axis.text.y = element_text(size=11),
+         axis.text.x = element_text(size=12, face="bold"),
+         axis.title.y = element_text(size=12, face="bold"))
+
+dev.off()
+
+# separate panels for nutrient treatments
+ggplot(farm_data) +
+   facet_grid(cols = vars(add_nuts),
+              drop = T) +
+   geom_col(aes(x = add_fish,
+                y = ch4_lake,
+                fill = add_fish),
+            size=1.25) +
+   scale_fill_manual(limits = c("none", "low", "high"),
+                     values = c("gray60", "seagreen3", "seagreen4"),
+                     name = "Fish density") +
+   xlab("") +
+   ylab("Methane (uM)") +
+   # theme_classic() +
+   theme(axis.text.x = element_blank(),
+         axis.text.y = element_text(size=11),
+         axis.title.y = element_text(size=12),
+         strip.background = element_rect(color="black", fill="gray80"),
+         strip.text = element_text(size=11, face="bold"))
+
+
+# Nitrous oxide
+
+png(file="Farm_data_N2O_prelim.png")
+# windows()
+
+ggplot(farm_data) +
+   geom_col(aes(x = factor(add_nuts),
+                y = n2o_lake,
+                fill = add_fish),
+            position="dodge2") +
+   scale_x_discrete(limits = c("control", "+ nuts"),
+                    labels = c("Control", "+ Nutrients")) +
+   scale_fill_manual(limits = c("none", "low", "high"),
+                     values = c("gray60", "seagreen3", "seagreen4"),
+                     name = "Fish density") +
+   xlab("") +
+   ylab("Nitrous Oxide (uM)") +
+   theme_classic() +
+   theme(axis.text.y = element_text(size=11),
+         axis.text.x = element_text(size=12, face="bold"),
+         axis.title.y = element_text(size=12, face="bold"))
+
+dev.off()
+
+
