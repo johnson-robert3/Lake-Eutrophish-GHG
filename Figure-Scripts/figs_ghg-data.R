1d26a5510f96315cffa5a4c7752183ad091c898a
Robert Johnson <johnson.robert.333@gmail.com>
2020-06-22 11:26:00 -0500

    added error bars to methanogenesis fig; added aes() to main plot calls
    **updated expand_scale() to expansion() for expanding plot axes

--- Figure-Scripts/figs_ghg-data.R
+++ Figure-Scripts/figs_ghg-data.R
@@ -12,18 +12,19 @@ library(patchwork)
 
 # METHANE
 windows(height=5, width=7)
-ggplot(lake_conc) +
-   geom_line(aes(x = doy, y = ch4_lake, alpha = pond_id), size=1.5, color="seagreen3") +
-   geom_point(aes(x = doy, y = ch4_lake), size=4, color="white") +
-   geom_point(aes(x = doy, y = ch4_lake, alpha = pond_id), size=4, color="seagreen3") +
+ggplot(lake_conc, aes(x = doy, y = ch4_lake)) +
+   # data
+   geom_line(aes(alpha = pond_id), size=1.5, color="seagreen3") +
+   geom_point(size=4, color="white") +
+   geom_point(aes(alpha = pond_id), size=4, color="seagreen3") +
    # scales and axes
    scale_alpha_manual(breaks = c("A", "B", "C", "D", "E", "F"),
                       values = c(0.25, 0.4, 0.55, 0.7, 0.85, 1),
                       name = "Pond") +
    scale_x_continuous(name = "DOY",
-                      expand = expand_scale(mult=0.1)) +
+                      expand = expansion(mult=0.1)) +
    scale_y_continuous(name = expression(Methane~(mu*M)),
-                      expand = expand_scale(mult=0.1)) +
+                      expand = expansion(mult=0.1)) +
    # aesthetics
    theme_classic()
 
@@ -31,6 +32,7 @@ ggplot(lake_conc) +
 # NITROUS OXIDE
 windows(height=5, width=7)
 ggplot(lake_conc, aes(x = doy, y = n2o_lake)) +
+   # data
    geom_line(aes(alpha = pond_id), size=1.5, color="dodgerblue3") +
    geom_point(size=4, color="white") +
    geom_point(aes(alpha = pond_id), size=4, color="dodgerblue3") +
@@ -39,9 +41,9 @@ ggplot(lake_conc, aes(x = doy, y = n2o_lake)) +
                       values = c(0.25, 0.4, 0.55, 0.7, 0.85, 1),
                       name = "Pond") +
    scale_x_continuous(name = "DOY",
-                      expand = expand_scale(mult=0.1)) +
+                      expand = expansion(mult=0.1)) +
    scale_y_continuous(name = expression(Nitrous~oxide~(mu*M)),
-                      expand = expand_scale(mult=0.1)) +
+                      expand = expansion(mult=0.1)) +
    expand_limits(y = 0) +
    # aesthetics
    theme_classic()
@@ -51,18 +53,28 @@ ggplot(lake_conc, aes(x = doy, y = n2o_lake)) +
 
 # METHANE
 windows(height=5, width=7)
-ggplot(methano_rates) +
-   geom_line(aes(x = doy, y = ch4_rate, alpha = pond_id), size=1.5, color="firebrick2") +
-   geom_point(aes(x = doy, y = ch4_rate), size=4, color="white") +
-   geom_point(aes(x = doy, y = ch4_rate, alpha = pond_id), size=4, color="firebrick2") +
+ggplot(methano_rates %>%
+          mutate(doy = case_when(.$pond_id=="A" ~ doy-0.2,
+                                 .$pond_id=="B" ~ doy-0.1,
+                                 .$pond_id=="C" ~ doy,
+                                 .$pond_id=="D" ~ doy+0.1,
+                                 .$pond_id=="E" ~ doy+0.2,
+                                 .$pond_id=="F" ~ doy+0.3)),
+       aes(x = doy, y = ch4_rate)) +
+   # data
+   geom_line(aes(alpha = pond_id), size=1.5, color="firebrick2") +
+   geom_errorbar(aes(x = doy, ymin = ch4_rate - sd_ch4_rate, ymax = ch4_rate + sd_ch4_rate), 
+                 color="gray60", width=0) +
+   geom_point(size=4, color="white") +
+   geom_point(aes(alpha = pond_id), size=4, color="firebrick2") +
    # scales and axes
    scale_alpha_manual(breaks = c("A", "B", "C", "D", "E", "F"),
                       values = c(0.25, 0.4, 0.55, 0.7, 0.85, 1),
                       name = "Pond") +
    scale_x_continuous(name = "DOY",
-                      expand = expand_scale(mult=0.1)) +
+                      expand = expansion(mult=0.1)) +
    scale_y_continuous(name = expression(Methane~production~(mu*mol~ml^-1*(s+w)~h^-1)),
-                      expand = expand_scale(mult=0.1)) +
+                      expand = expansion(mult=0.1)) +
    expand_limits(y = 0) +
    # aesthetics
    theme_classic()
