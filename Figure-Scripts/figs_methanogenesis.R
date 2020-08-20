3a060f8ab08497328f6421a5b3956f8d28cd46cd
Robert Johnson <johnson.robert.333@gmail.com>
2020-08-20 12:49:44 -0500

    added CO2 2-panel fig

--- Figure-Scripts/figs_methanogenesis.R
+++ Figure-Scripts/figs_methanogenesis.R
@@ -363,10 +363,50 @@ ggsave(filename = "Figures/methanogenesis-co2_3panel.png", height=10, width=6, u
 {# Comparing between food web treatments within each nutrient treatment
 
 # REFERENCE
+# windows()
+c.ref =
+ggplot(methano_rates %>% left_join(pond_data) %>% filter(!(is.na(co2_rate))) %>% filter(trt_nutrients=="no"),
+       aes(x = doy, y = co2_rate)) +
+   geom_line(aes(alpha = trt_fish), size=1.25, color="cornflowerblue", show.legend=F) +
+   geom_point(size=4, color="white") +
+   geom_point(aes(alpha = trt_fish), shape=21, size=4, fill="cornflowerblue", color="royalblue") +
+   #
+   scale_alpha_manual(name = "Benthic-Pelagic \nCoupling",
+                      breaks = c("high", "medium", "low"),
+                      values = c("high" = 0.9, "medium" = 0.6, "low" = 0.3),
+                      labels = c("high" = "High", "medium" = "Intermediate", "low" = "Low")) +
+   scale_x_continuous(name = "DOY", expand = expansion(mult=0.1)) +
+   scale_y_continuous(name = expression(CO[2]~production~(mu*mol~g^-1~h^-1)), expand = expansion(mult=0.1)) +
+   labs(title = "Reference") +
+   theme_classic()
 
 
 # PULSED
+# windows()
+c.pul =
+ggplot(methano_rates %>% left_join(pond_data) %>% filter(!(is.na(co2_rate))) %>% filter(trt_nutrients=="yes"),
+       aes(x = doy, y = co2_rate)) +
+   geom_vline(xintercept = c(176, 211), color="gray40", linetype=2) +
+   geom_line(aes(alpha = trt_fish), size=1.25, color="seagreen3", show.legend=F) +
+   geom_point(size=4, color="white") +
+   geom_point(aes(alpha = trt_fish), shape=21, size=4, fill="seagreen3", color="seagreen") +
+   #
+   scale_alpha_manual(name = "Benthic-Pelagic \nCoupling",
+                      breaks = c("high", "medium", "low"),
+                      values = c("high" = 0.9, "medium" = 0.6, "low" = 0.3),
+                      labels = c("high" = "High", "medium" = "Intermediate", "low" = "Low")) +
+   scale_x_continuous(name = "DOY", expand = expansion(mult=0.1)) +
+   scale_y_continuous(name = expression(CO[2]~production~(mu*mol~g^-1~h^-1)), expand = expansion(mult=0.1)) +
+   labs(title = "Pulsed") +
+   theme_classic()
 
+
+# 2-panel
+windows(height=7, width=6)
+
+c.ref / c.pul
+
+ggsave(filename = "Figures/methanogenesis-co2_2panel.png", height=7, width=6, units="in")
 
 }
 
