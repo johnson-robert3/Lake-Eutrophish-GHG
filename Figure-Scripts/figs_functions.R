77b7209b748811d44af22792a25c39c0866d9e33
Robert Johnson <johnson.robert.333@gmail.com>
2020-10-28 17:21:07 -0500

    initial commit

---
+++ Figure-Scripts/figs_functions.R
@@ -0,0 +1,54 @@
+#~~~
+# Script to house all custom functions used for figures
+# By: Robert Johnson
+#~~~
+
+
+# 3-panel figures (by food web treatment)
+fig_aes_fw = function(.fig) {
+   
+   .fig +
+      # pulse
+      geom_vline(xintercept = c(176, 211), linetype=2, color="gray40") +
+      ## data as points
+      # geom_line(aes(group = trt_nutrients), size=0.5) +
+      # geom_point(color="white", fill="white", shape=21, size=3) +
+      # geom_point(aes(fill = trt_nutrients), shape=21, size=3, alpha=0.8) +
+      # scale_fill_manual(name = NULL,
+      #                   labels = c("no" = "Reference", "yes" = "Pulsed"),
+      #                   values = c("no" = "cornflowerblue", "yes" = "seagreen3")) +
+      ## data as lines
+      geom_line(aes(group = trt_nutrients, color = trt_nutrients), size=1.2, alpha=0.7) +
+      scale_color_manual(name = NULL,
+                         labels = c("no" = "Reference", "yes" = "Pulsed"),
+                         values = c("no" = "cornflowerblue", "yes" = "seagreen3")) +
+      ##
+      scale_x_continuous(name = "DOY", expand = expansion(mult=0.1)) +
+      theme_classic()
+   
+}
+
+
+
+# 2-panel figures (by nutrient treatment)
+
+
+
+
+# Buoyancy Frequency function for non-time-series data
+profile_bf = function(.dat) {
+   
+   bf = buoyancy.freq(wtr = .dat$temp,
+                      depths = .dat$depth_int)
+   
+   buoy_freq = as.vector(bf)
+   
+   depth = attr(bf, "depths")
+   
+   table = data.frame(depth, buoy_freq)
+   
+   return(table)
+   
+}
+
+
