0a006cec4b18113f3a44ccf30f7a4d7af3323aa9
Robert Johnson <johnson.robert.333@gmail.com>
2020-11-12 11:35:45 -0600

    initial commit; 2-panel barplot of ebullitive flux

---
+++ Figure-Scripts/figs_ebullition.R
@@ -0,0 +1,168 @@
+#~~~
+# Ebullition figures
+# By: Robert Johnson
+#~~~
+
+
+library(cowplot)
+library(patchwork)
+
+source("Figure-Scripts/figs_functions.R")
+
+
+windows()
+ggplot(ebu_flux_pond,
+       aes(x = doy, y = ch4_ebu_flux)) +
+   geom_line(aes(color = pond_id, group = pond_id)) +
+   geom_point(aes(color = pond_id), size=1.5) +
+   theme_classic()
+
+
+# points
+windows()
+ggplot(ebu_flux_pond %>%
+          left_join(pond_data) %>%
+          group_by(trt_nutrients, doy) %>%
+          summarize(n = n(),
+                    se_ebu = sd(ch4_ebu_flux)/sqrt(n),
+                    mean_ebu = mean(ch4_ebu_flux)) %>%
+          ungroup() %>%
+          mutate(doy = if_else(trt_nutrients=="yes", doy+1, doy)),
+       aes(x = doy, y = mean_ebu)) +
+   geom_line(aes(color = trt_nutrients)) +
+   geom_point(aes(color = trt_nutrients), size = 1.5) +
+   geom_errorbar(aes(ymin = mean_ebu - se_ebu,
+                     ymax = mean_ebu + se_ebu),
+                 color = "gray60", width=0) +
+   theme_classic()
+
+
+# bars
+windows()
+ggplot(ebu_flux_pond %>%
+          left_join(pond_data) %>%
+          # mmol
+          mutate(ch4_ebu_flux = ch4_ebu_flux/1000) %>%
+          group_by(trt_nutrients, doy) %>%
+          summarize(n = n(),
+                    se_ebu = sd(ch4_ebu_flux)/sqrt(n),
+                    mean_ebu = mean(ch4_ebu_flux)) %>%
+          ungroup(),
+       aes(x = doy, y = mean_ebu, group = trt_nutrients)) +
+   #
+   geom_errorbar(aes(ymin = mean_ebu - se_ebu,
+                     ymax = mean_ebu + se_ebu),
+                 position = position_dodge(width = 0.9*6),
+                 width=2, color="gray50") +
+   #
+   geom_col(aes(fill = trt_nutrients),
+            position = position_dodge()) +
+   #
+   #
+   scale_y_continuous(name = expression(Ebullitive~flux~(mmol~m^-2~d^-1))) +
+   theme_classic()
+
+
+## 2 panel points
+
+# Reference
+r =
+# windows()
+ggplot(ebu_flux_pond %>%
+          left_join(pond_data) %>%
+          # mmol
+          mutate(ch4_ebu_flux = ch4_ebu_flux/1000) %>%
+          filter(trt_nutrients=="no") %>%
+          group_by(doy) %>%
+          summarize(n = n(),
+                    se = sd(ch4_ebu_flux)/sqrt(n),
+                    mean = mean(ch4_ebu_flux)) %>%
+          ungroup(),
+       aes(x = doy, y = mean)) +
+   geom_hline(yintercept=0, linetype=2) +
+   geom_errorbar(aes(ymin = mean - se, ymax = mean + se), width=0, color="gray60") +
+   geom_point(color = "cornflowerblue", size=3) +
+   geom_line(color = "cornflowerblue") +
+   theme_classic()
+
+# Pulsed
+p =
+# windows()
+ggplot(ebu_flux_pond %>%
+          left_join(pond_data) %>%
+          # mmol
+          mutate(ch4_ebu_flux = ch4_ebu_flux/1000) %>%
+          filter(trt_nutrients=="yes") %>%
+          group_by(doy) %>%
+          summarize(n = n(),
+                    se = sd(ch4_ebu_flux)/sqrt(n),
+                    mean = mean(ch4_ebu_flux)) %>%
+          ungroup(),
+       aes(x = doy, y = mean)) +
+   geom_hline(yintercept=0, linetype=2) +
+   geom_errorbar(aes(ymin = mean - se, ymax = mean + se), width=0, color="gray60") +
+   geom_point(color = "seagreen3", size=3) +
+   geom_line(color = "seagreen3") +
+   theme_classic()
+
+
+windows(); r / p
+
+
+
+# 2 panel bars
+
+# reference 
+r = 
+ggplot(ebu_flux_pond %>%
+          left_join(pond_data) %>%
+          # mmol
+          mutate(ch4_ebu_flux = ch4_ebu_flux/1000) %>%
+          filter(trt_nutrients=="no") %>%
+          group_by(doy) %>%
+          summarize(n = n(),
+                    se = sd(ch4_ebu_flux)/sqrt(n),
+                    mean = mean(ch4_ebu_flux)) %>%
+          ungroup(),
+       aes(x = doy, y = mean)) +
+   geom_errorbar(aes(ymin = mean - se, 
+                     ymax = mean + se),
+                 color="gray50", width=0) +
+   #
+   geom_col(fill = "cornflowerblue", color = "gray50") +
+   # 
+   scale_x_continuous(name = "DOY") +
+   scale_y_continuous(name = expression(CH[4]~ebullition~(mmol~m^-2~d^-1)), limits = c(0, 15)) +
+   ggtitle("Reference") +
+   theme_classic()
+
+
+# pulse
+p = 
+ggplot(ebu_flux_pond %>%
+          left_join(pond_data) %>%
+          # mmol
+          mutate(ch4_ebu_flux = ch4_ebu_flux/1000) %>%
+          filter(trt_nutrients=="yes") %>%
+          group_by(doy) %>%
+          summarize(n = n(),
+                    se = sd(ch4_ebu_flux)/sqrt(n),
+                    mean = mean(ch4_ebu_flux)) %>%
+          ungroup(),
+       aes(x = doy, y = mean)) +
+   geom_errorbar(aes(ymin = mean - se, 
+                     ymax = mean + se),
+                 color="gray50", width=0) +
+   #
+   geom_col(fill = "seagreen3", color = "gray50") +
+   # 
+   scale_x_continuous(name = "DOY") +
+   scale_y_continuous(name = expression(CH[4]~ebullition~(mmol~m^-2~d^-1)), limits = c(0, 15)) +
+   ggtitle("Pulsed") +
+   theme_classic()
+
+
+windows(); r / p
+
+ggsave(filename = "Figures/new-figs/ebullition_ch4.png", height=7, width=6, units="in")
+
