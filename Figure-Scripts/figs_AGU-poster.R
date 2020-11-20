ffaf81d9d8373d9486dde4ca473ba7a0a86ac16a
Robert Johnson <johnson.robert.333@gmail.com>
2020-11-20 11:11:35 -0600

    changed trt colors and added loess smooth line to diffusive flux fig

--- Figure-Scripts/figs_AGU-poster.R
+++ Figure-Scripts/figs_AGU-poster.R
@@ -21,24 +21,19 @@ source("Figure-Scripts/figs_functions.R")
 windows()
 a =
 ggplot(lake_flux %>%
-          left_join(pond_data),# %>%
-          # mutate(co2_flux_eqv = co2_flux_eqv / 1000) %>%
-          # group_by(trt_nutrients, doy) %>%
-          # summarize(mean = mean(co2_flux),
-          #           se = sd(co2_flux)/sqrt(n())) %>%
-          # ungroup(),
+          left_join(pond_data),
        aes(x = doy, y = co2_flux)) +
    #
    geom_hline(yintercept=0, linetype=3, color="gray60") +
    geom_vline(xintercept = c(176, 211), linetype=2, color="gray60") +
-   #
-   # geom_errorbar(aes(x = doy, ymin = mean-se, ymax = mean+se), width=1, color="gray70") +
-   geom_line(aes(color = trt_nutrients, group = pond_id), alpha=0.6, size=1.25) +
-   geom_point(aes(color = trt_nutrients), size=1.5, alpha=0.6) +
+   # pond data
+   geom_line(aes(color = trt_nutrients, group = pond_id), alpha=0.3, size=1) +
+   # geom_point(aes(color = trt_nutrients), size=1.5, alpha=0.4) +
+   # treatment mean (loess smooth)
+   geom_smooth(aes(color = trt_nutrients), size=1.5, alpha=0.8, se=F, span=0.15) +
    #
    scale_color_manual(name = NULL, breaks = nut_breaks, values = nut_color, labels = nut_labs) +
    scale_x_continuous(name = "DOY") +
-   # scale_y_continuous(name = expression(g~CO[2]*"-eq"~m^-2~d^-1)) +
    scale_y_continuous(name = expression(CO[2]~flux~(mmol~m^-2~d^-1))) +
    #
    ggtitle(expression(CO[2])) +
@@ -49,24 +44,19 @@ ggplot(lake_flux %>%
 windows()
 b =
 ggplot(lake_flux %>%
-          left_join(pond_data),# %>%
-          # mutate(ch4_flux_eqv = ch4_flux_eqv / 1000) %>%
-          # group_by(trt_nutrients, doy) %>%
-          # summarize(mean = mean(ch4_flux),
-          #           se = sd(ch4_flux)/sqrt(n())) %>%
-          # ungroup(),
+          left_join(pond_data),
        aes(x = doy, y = ch4_flux)) +
    #
    geom_hline(yintercept=0, linetype=3, color="gray60") +
    geom_vline(xintercept = c(176, 211), linetype=2, color="gray60") +
-   #
-   # geom_errorbar(aes(x = doy, ymin = mean-se, ymax = mean+se), width=1, color="gray70") +
-   geom_line(aes(color = trt_nutrients, group = pond_id), alpha=0.6, size=1.25) +
-   geom_point(aes(color = trt_nutrients), size=1.5, alpha=0.6) +
+   # pond data
+   geom_line(aes(color = trt_nutrients, group = pond_id), alpha=0.3, size=1) +
+   # geom_point(aes(color = trt_nutrients), size=1.5, alpha=0.4) +
+   # treatment mean (loess smooth)
+   geom_smooth(aes(color = trt_nutrients), size=1.5, alpha=0.8, se=F, span=0.15) +
    #
    scale_color_manual(name = NULL, breaks = nut_breaks, values = nut_color, labels = nut_labs) +
    scale_x_continuous(name = "DOY") +
-   # scale_y_continuous(name = expression(g~CO[2]*"-eq"~m^-2~d^-1)) +
    scale_y_continuous(name = expression(CH[4]~flux~(mmol~m^-2~d^-1))) +
    #
    ggtitle(expression(CH[4])) +
@@ -78,23 +68,19 @@ windows()
 c =
 ggplot(lake_flux %>%
           left_join(pond_data) %>%
-          mutate(n2o_flux = n2o_flux * 1000),# %>%
-          # group_by(trt_nutrients, doy) %>%
-          # summarize(mean = mean(n2o_flux),
-          #           se = sd(n2o_flux)/sqrt(n())) %>%
-          # ungroup(),
+          mutate(n2o_flux = n2o_flux * 1000), 
        aes(x = doy, y = n2o_flux)) +
    #
    geom_hline(yintercept=0, linetype=3, color="gray60") +
    geom_vline(xintercept = c(176, 211), linetype=2, color="gray60") +
-   #
-   # geom_errorbar(aes(x = doy, ymin = mean-se, ymax = mean+se), width=1, color="gray70") +
-   geom_line(aes(color = trt_nutrients, group = pond_id), alpha=0.6, size=1.25) +
-   geom_point(aes(color = trt_nutrients), size=1.5, alpha=0.6) +
+   # pond data
+   geom_line(aes(color = trt_nutrients, group = pond_id), alpha=0.3, size=1) +
+   # geom_point(aes(color = trt_nutrients), size=1.5, alpha=0.4) +
+   # treatment mean (loess smooth)
+   geom_smooth(aes(color = trt_nutrients), size=1.5, alpha=0.8, se=F, span=0.15) +
    #
    scale_color_manual(name = NULL, breaks = nut_breaks, values = nut_color, labels = nut_labs) +
    scale_x_continuous(name = "DOY") +
-   # scale_y_continuous(name = expression(mg~CO[2]*"-eq"~m^-2~d^-1)) +
    scale_y_continuous(name = expression(N[2]*O~flux~(mu*mol~m^-2~d^-1)),
                       limits = c(-4, 3),
                       breaks = seq(-4, 3, 1)) +
@@ -121,13 +107,15 @@ ggplot(lake_flux %>% left_join(pond_data) %>% filter(trt_nutrients=="yes"),
    geom_vline(xintercept = c(176, 211), color="gray40", linetype=2) +
    geom_hline(yintercept = 0, linetype=3, color="gray40") +
    #
-   geom_line(aes(group=pond_id), color="seagreen3", alpha=0.6, size=1.25) +
-   geom_point(aes(shape = pond_id), color="seagreen3", size=2, alpha=0.6) +
+   geom_line(aes(group=pond_id), color="seagreen3", alpha=0.4, size=1) +
+   geom_point(aes(shape = pond_id), color="seagreen3", size=2.25, alpha=0.4) +
+   geom_smooth(se=F, color="seagreen", size=1.25, alpha=0.8, span = 0.2) +
    #
    scale_shape_manual(name = "Pond",
                       breaks = c('A', 'B', 'C'),
-                      values = c("A" = 1, "B" = 2, "C" = 0)) +
-   scale_x_continuous(name = "DOY", expand = expansion(mult=0.1)) +
+                      values = c("A" = 1, "B" = 2, "C" = 0),
+                      guide = guide_legend(override.aes = list(size=3))) +
+   scale_x_continuous(name = "DOY", expand = expansion(mult=0.1), breaks=seq(145,240,15)) +
    scale_y_continuous(name = expression(CO[2]~flux~(mmol~m^-2~d^-1)), limits = c(-25, 250)) +
    labs(title = "Pulsed") +
    theme_classic()
@@ -140,8 +128,9 @@ ggplot(lake_flux %>% left_join(pond_data) %>% filter(trt_nutrients=="no"),
        aes(x = doy, y = co2_flux)) +
    geom_hline(yintercept = 0, linetype=3, color="gray40") +
    #
-   geom_line(aes(group=pond_id), color="cornflowerblue", alpha=0.6, size=1.25) +
-   geom_point(aes(shape = pond_id), color="cornflowerblue", size=2, alpha=0.6) +
+   geom_line(aes(group=pond_id), color="cornflowerblue", alpha=0.6, size=1) +
+   geom_point(aes(shape = pond_id), color="cornflowerblue", size=2.25, alpha=0.6) +
+   geom_smooth(se=F, color="royalblue3", size=1.25, alpha=0.8, span = 0.2) +
    #
    scale_shape_manual(name = "Pond",
                       breaks = c('D', 'E', 'F'),
@@ -329,7 +318,7 @@ ggplot(lake_flux %>%
    geom_vline(xintercept = c(176, 211), linetype=2, color="gray60") +
    #
    geom_line(aes(color = trt_nutrients, group=pond_id), alpha=0.6, size=1) +
-   geom_point(aes(color = trt_nutrients, shape = pond_id), size=1.75, alpha=0.6) +
+   geom_point(aes(color = trt_nutrients, shape = pond_id), size=1.75, alpha=0.6, show.legend=c(shape=T, color=F)) +
    #
    scale_color_manual(name = NULL, breaks = nut_breaks, values = nut_color, labels = nut_labs) +
    scale_shape_manual(name = "Pond",
