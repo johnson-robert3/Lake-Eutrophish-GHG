d55fd66091b1ff15f0bc6a70f1d8431bf81d3a90
Robert Johnson <johnson.robert.333@gmail.com>
2022-03-22 15:58:02 -0500

    updates, added figs to visually inspect data for each pond

--- Figure-Scripts/figs_minidot.R
+++ Figure-Scripts/figs_minidot.R
@@ -8,55 +8,171 @@ library(slider)
 library(patchwork)
 
 
+### View MiniDOT raw data
 
-# miniDOT
-
+# Pond A
 windows()
-ggplot(minidot %>% filter(pond_id=="A", doy %in% c(180:191)) %>% 
-          mutate(newdo = slide_dbl(do_sat, ~mean(.), .before=4, .complete=F))) +
-   # geom_line(aes(x = date_time, y = temp), color="firebrick2") +
-   geom_line(aes(x = date_time, y = do_sat), color="cornflowerblue") +
+ggplot(minidot %>% 
+          filter(pond_id=="A") %>% 
+          
+          # add rolling window of do_sat data
+          mutate(rolldo = slide_dbl(do, ~mean(.), .before=5, .complete=F)) %>%
+          
+          # add day-start for vertical lines
+          mutate(hour = hour(date_time),
+                 minute = minute(date_time)) %>%
+          # select which days to view
+          filter(doy %in% c(162:165))) +
+   
+   # data as lines
+   geom_line(aes(x = date_time, y = do), color="cornflowerblue") +
+   # data as points
+   geom_point(aes(x = date_time, y = do), color="cornflowerblue") +
+   # denote days
+   geom_vline(data = ~filter(.x, hour==0 & minute==0), aes(xintercept = date_time), linetype=2, color="gray60") +
+   
+   # rolling window data
+   geom_line(aes(x = date_time, y = rolldo), color="firebrick2") +
+   geom_point(aes(x = date_time, y = rolldo), color="firebrick2") +
+   
    theme_classic()
 
 
-
-
-# Sonde, top 15cm
-
+# Pond B
 windows()
-ggplot(sonde_surface %>% filter(pond_id=="A")) +
-   geom_line(aes(x = doy, y = do_sat), color="firebrick2") +
-   geom_line(data = minidot %>% filter(pond_id=="A") %>% group_by(doy) %>% summarize(do_sat = mean(do_sat, na.rm=T)) %>% ungroup(),
-             aes(x = doy, y = do_sat), color="cornflowerblue") +
+ggplot(minidot %>% 
+          filter(pond_id=="B") %>% 
+          
+          # add rolling window of do_sat data
+          # mutate(rolldo = slide_dbl(do_sat, ~mean(.), .before=4, .complete=F)) %>%
+          
+          # add day-start for vertical lines
+          mutate(hour = hour(date_time),
+                 minute = minute(date_time)) %>%
+          # select which days to view
+          filter(doy %in% c(210:214))) +
+   
+   # data as lines
+   geom_line(aes(x = date_time, y = do), color="cornflowerblue") +
+   # data as points
+   geom_point(aes(x = date_time, y = do), color="cornflowerblue") +
+   # denote days
+   geom_vline(data = ~filter(.x, hour==0 & minute==0), aes(xintercept = date_time), linetype=2, color="gray60") +
+   
+   theme_classic()
+
+
+# Pond C
+windows()
+ggplot(minidot %>% 
+          filter(pond_id=="C") %>% 
+          
+          # add rolling window of do_sat data
+          # mutate(rolldo = slide_dbl(do_sat, ~mean(.), .before=4, .complete=F)) %>%
+          
+          # add day-start for vertical lines
+          mutate(hour = hour(date_time),
+                 minute = minute(date_time)) %>%
+          # select which days to view
+          filter(doy %in% c(210:214))) +
+   
+   # data as lines
+   geom_line(aes(x = date_time, y = do), color="cornflowerblue") +
+   # data as points
+   geom_point(aes(x = date_time, y = do), color="cornflowerblue") +
+   # denote days
+   geom_vline(data = ~filter(.x, hour==0 & minute==0), aes(xintercept = date_time), linetype=2, color="gray60") +
+   
+   theme_classic()
+
+
+# Pond D
+windows()
+ggplot(minidot %>% 
+          filter(pond_id=="D") %>% 
+          
+          # add rolling window of do_sat data
+          # mutate(rolldo = slide_dbl(do_sat, ~mean(.), .before=4, .complete=F)) %>%
+          
+          # add day-start for vertical lines
+          mutate(hour = hour(date_time),
+                 minute = minute(date_time)) %>%
+          # select which days to view
+          filter(doy %in% c(210:214))) +
+   
+   # data as lines
+   geom_line(aes(x = date_time, y = do), color="cornflowerblue") +
+   # data as points
+   geom_point(aes(x = date_time, y = do), color="cornflowerblue") +
+   # denote days
+   geom_vline(data = ~filter(.x, hour==0 & minute==0), aes(xintercept = date_time), linetype=2, color="gray60") +
+   
+   theme_classic()
+
+
+# Pond E
+windows()
+ggplot(minidot %>% 
+          filter(pond_id=="E") %>% 
+          
+          # add rolling window of do_sat data
+          # mutate(rolldo = slide_dbl(do_sat, ~mean(.), .before=4, .complete=F)) %>%
+          
+          # add day-start for vertical lines
+          mutate(hour = hour(date_time),
+                 minute = minute(date_time)) %>%
+          # select which days to view
+          filter(doy %in% c(210:214))) +
+   
+   # data as lines
+   geom_line(aes(x = date_time, y = do), color="cornflowerblue") +
+   # data as points
+   geom_point(aes(x = date_time, y = do), color="cornflowerblue") +
+   # denote days
+   geom_vline(data = ~filter(.x, hour==0 & minute==0), aes(xintercept = date_time), linetype=2, color="gray60") +
+   
+   theme_classic()
+
+
+# Pond F
+windows()
+ggplot(minidot %>% 
+          filter(pond_id=="F") %>% 
+          
+          # add rolling window of do_sat data
+          # mutate(rolldo = slide_dbl(do_sat, ~mean(.), .before=4, .complete=F)) %>%
+          
+          # add day-start for vertical lines
+          mutate(hour = hour(date_time),
+                 minute = minute(date_time)) %>%
+          # select which days to view
+          filter(doy %in% c(210:214))) +
+   
+   # data as lines
+   geom_line(aes(x = date_time, y = do), color="cornflowerblue") +
+   # data as points
+   geom_point(aes(x = date_time, y = do), color="cornflowerblue") +
+   # denote days
+   geom_vline(data = ~filter(.x, hour==0 & minute==0), aes(xintercept = date_time), linetype=2, color="gray60") +
+   
    theme_classic()
 
 
 
-# six panel, minidot vs sonde DO
-
-
-# windows()
-# a =
-ggplot(sonde_surface %>% filter(pond_id=="A")) +
-   geom_line(aes(x = doy, y = do_sat), color="firebrick2") +
-   geom_line(data = minidot %>% filter(pond_id=="A") %>% group_by(doy) %>% summarize(do_sat = mean(do_sat, na.rm=T)) %>% ungroup(),
-             aes(x = doy, y = do_sat), color="cornflowerblue") +
-   theme_classic()
-
-
-# windows(height=6, width=10)
-# 
-# (a + b + c) / (d + e + f)
 
 
 
-# wrapped
+
+
+
+
+# six panel, miniDOT vs sonde profile DO
 
 windows()
 ggplot(sonde_surface) +
    # daily sonde
    geom_line(aes(x = doy, y = do_sat), color="firebrick2") +
-   # morning minidot (between 9am and noon)
+   # morning minidot (between 9am and 11am)
    geom_line(data = minidot %>% 
                 mutate(hour = hour(date_time)) %>% 
                 filter(hour>=9, hour<=11) %>% 
