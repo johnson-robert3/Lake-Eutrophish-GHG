3a012f76ba5f9411763ee04444992989ad298dba
Robert Johnson <johnson.robert.333@gmail.com>
2022-04-08 18:25:07 -0500

    update file paths

--- Data-Wrangling/edi-data-output.R
+++ Data-Wrangling/edi-data-output.R
@@ -9,13 +9,13 @@ hobo_strat %>%
    relocate(doy, .after=date_time) %>%
    mutate(across(meta_top:thermocline, ~na_if(., "NaN"))) %>%
    mutate(across(meta_top:buoy_freq, ~replace(., is.na(.), "mix"))) %>%
-   write.csv(., file = "Data/EDI-Data/stratification_data.csv", row.names=FALSE)
+   write.csv(., file = "Data/stratification_data.csv", row.names=FALSE)
 
 
 
 # Weather station
 weather_data %>%
-   write.csv(., file = "Data/EDI-Data/weather-station_data.csv", row.names=FALSE)
+   write.csv(., file = "Data/weather-station_data.csv", row.names=FALSE)
 
 
 
@@ -24,13 +24,13 @@ lake_flux %>%
    select(pond_id, date, doy, surface_temp, ends_with("lake"), ch4_atmo:n2o_atmo, k_cole, ends_with("flux")) %>%
    relocate(n2o_lake, .after=co2_lake) %>%
    rename(k600 = k_cole) %>%
-   write.csv(., file = "Data/EDI-Data/greenhouse-gas_data.csv", row.names=FALSE)
+   write.csv(., file = "Data/greenhouse-gas_data.csv", row.names=FALSE)
 
 
 
 # Metabolism
 metab_mle %>%
-   write.csv(., file = "Data/EDI-Data/daily-metabolism_data.csv", row.names=FALSE)
+   write.csv(., file = "Data/daily-metabolism_data.csv", row.names=FALSE)
 
 
 
