733efabde8b1cee0f45e325424b67d66d29e26cc
Robert Johnson <johnson.robert.333@gmail.com>
2022-04-25 15:40:22 -0500

    added initial step for processing noisy DO data

--- Data-Wrangling/metabolism-calcs.R
+++ Data-Wrangling/metabolism-calcs.R
@@ -5,6 +5,7 @@
 
 
 library(LakeMetabolizer)
+library(slider)
 
 
 #---
@@ -13,7 +14,42 @@ library(LakeMetabolizer)
 
 # Get and combine necessary data from various data sets
 
-# Temp and DO from miniDOTs
+
+#-- Step 1: Address noisy DO time-series
+minidot = minidot %>% 
+   group_by(pond_id) %>% 
+   arrange(date_time, .by_group=TRUE) %>%
+   
+   # identify large drops in DO (caused by water column turnover)
+   # add variable for change in DO concentration from previous point
+   mutate(delta_do = do - lag(do)) %>%
+   
+   # remove the offending point along with the next 2.5 hours (5 points) of data any time DO drops more than 2.0 mg/l
+   mutate(drop_pt = case_when(delta_do <= -2.0 ~ 1,
+                              TRUE ~ 0),
+          # corrected DO concentration
+          corr_do = case_when(drop_pt == 1 | 
+                                 lag(drop_pt, n=1) == 1 | 
+                                 lag(drop_pt, n=2) == 1 | 
+                                 lag(drop_pt, n=3) == 1 | 
+                                 lag(drop_pt, n=4) == 1 | 
+                                 lag(drop_pt, n=5) == 1 ~ -9999,
+                              TRUE ~ do),
+          corr_do = na_if(corr_do, -9999),
+          # linearly interpolate across removed points
+          corr_do = zoo::na.approx(corr_do, na.rm=FALSE)) %>%
+   
+   # another method for dealing with noisy DO time-series data
+   # 3-hour rolling window data
+   mutate(
+      # rolling window on original/raw data
+      roll_do = slide_dbl(do, ~mean(.), .before=3, .after=2, .complete=F),
+      # rolling window on new, corrected DO data (with large drops removed/interpolated)
+      roll_corr_do = slide_dbl(corr_do, ~mean(.), .before=3, .after=2, .complete=F)) %>%
+   ungroup()
+
+
+#-- Step 2: Add weather and surface limno data
 metab_data = minidot %>%
    # add weather
    left_join(weather_data) %>%
@@ -35,40 +71,39 @@ metab_data = minidot %>%
    select(-do_sat, -gust_speed)
 
 
-# Add mixed-layer depth
+#-- Step 3: Add mixed-layer depth
 metab_data = hobo_strat %>%
-   
-   # use thermocline for the mixed-layer depth (mld)
-   # select(pond_id, date_time, thermocline) %>%
+   # use thermocline for the mixed-layer depth 
    rename(z_mix = thermocline) %>%
    
-   # Address times of turnover and mixing in the ponds
-   
-   #  set 'mld' to 1.75m during times when ponds were mixed 
-   #  "NaN"s in the data are also turnover, when the rLakeAnalyzer functions could not calculate the stratification variables
-   mutate(z_mix = case_when(z_mix == "NaN" ~ 1.75,
-                            meta_top==0 & meta_bottom==2 ~ 1.75,
-                            z_mix == 0.125 ~ 1.75,
-                            TRUE ~ z_mix)) %>%
+   # Set Z_mix to 1.75m during times of turnover and mixing within the ponds  
+   mutate(z_mix = case_when(
+      # "NaN"s in the data (when rLakeAnalyzer functions can't calculate stratification variables) are also when water column is mixed
+      z_mix == "NaN" ~ 1.75,
+      # when the entire water column was considered Metalimnion
+      meta_top==0 & meta_bottom==2 ~ 1.75,
+      # rLakeAnalyzer functions seem to assign thermocline to 0.125m during times of mixing or weak, transient stratification 
+      z_mix == 0.125 ~ 1.75,
+      TRUE ~ z_mix)) %>%
    
    # add a treatment ID 
    #  data for B and F will be used for all ponds in a treatment; t-chains were only deployed in ponds B and F
    mutate(trt = case_when(.$pond_id=="B" ~ "pulse",
                           .$pond_id=="F" ~ "ref")) %>%
    select(-pond_id) %>%
-   #
+   
+   # add z_mix data back onto the metab dataset
    right_join(metab_data %>%
                  # treatment ID for combining
                  mutate(trt = case_when(.$pond_id %in% c("A", "B", "C") ~ "pulse",
                                         .$pond_id %in% c("D", "E", "F") ~ "ref"))) %>%
-   #
    relocate(pond_id) %>%
    relocate(z_mix, .after = salinity) %>%
    select(-trt) %>%
    arrange(pond_id, doy)
 
 
-# Additional parameters for metabolism calcs
+#-- Step 4: Additional parameters for metabolism calculations
 metab_data = metab_data %>%
    mutate(U10 = wind_speed * ((10 / wind_z)^(1/7)),
           k_cole = k.cole.base(U10),
