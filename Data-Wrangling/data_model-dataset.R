17932714d52edde2eaa2a0649ba739bf47b8b996
Robert Johnson <johnson.robert.333@gmail.com>
2022-04-29 13:45:11 -0500

    updates; add alkalinity, remove buoy_freq

--- Data-Wrangling/data_model-dataset.R
+++ Data-Wrangling/data_model-dataset.R
@@ -5,15 +5,15 @@
 # Needed data sets
 
 # lake_flux  [co2, ch4, and n2o flux]
-# metab_mle  [nep]
+# metabolism  [nep]
 # limno_field_data  [nutrients]
 # sonde_bottom  [SWI DO]
 # sonde_surface  [Chla, temp]
-# wind_k OR weather_data  [wind speed]
+# weather_data  [wind speed]
 # dea_rates  [DEA N2O]
 # methano_rates  [methanogenesis]
-# hobo_mld  [buoyancy frequency]
-
+# hobo_strat  [buoyancy frequency]  ### we are no longer calculating/using buoyancy frequency. physical limnologists say this is not a good/reliable/useful variable
+# alk_data  [alkalinity]
 
 
 ## Prep the individual data sets
@@ -26,8 +26,8 @@ t10 = lake_conc %>%
    select(pond_id, doy, ends_with("lake"))
 
 
-t2 = metab_mle %>%
-   filter(!(GPP<0 | R>0))
+t2 = metabolism %>%
+   filter(!(GPP < 0 | R > 0))
 
 
 t3 = limno_field_data %>%
@@ -48,11 +48,6 @@ t5 = sonde_profiles %>%
    select(pond_id, doy, temp, chla)
 
 
-# t6 = wind_k %>%
-#    select(doy, wnd, U10) %>%
-#    rename(wind_speed = wnd,
-#           wind_U10 = U10)
-
 t6 = weather_data %>%
    mutate(U10 = wind_speed * ((10 / wind_z)^(1/7))) %>%
    select(doy, wind_speed, U10) %>%
@@ -72,11 +67,16 @@ t8 = methano_rates %>%
    rename(methanogenesis = ch4_rate)
 
 
-t9 = hobo_mld %>%
-   mutate(doy = yday(date_time)) %>%
-   group_by(pond_id, doy) %>%
-   summarize(buoy_freq = median(buoy_freq, na.rm=TRUE)) %>%
-   ungroup()
+t9 = alk_data %>%
+   select(-sample_id, -notes) %>%
+   relocate(pond_id)
+
+
+# t9 = hobo_strat %>%
+#    mutate(doy = yday(date_time)) %>%
+#    group_by(pond_id, doy) %>%
+#    summarize(buoy_freq = median(buoy_freq, na.rm=TRUE)) %>%
+#    ungroup()
 
 
 # combined
@@ -88,23 +88,29 @@ test = full_join(t1, t10) %>%
    full_join(t5) %>%
    full_join(t6) %>%
    full_join(t7) %>%
-   full_join(t8)
+   full_join(t8) %>%
+   full_join(t9)
 
 
-test2 = test %>%
-   mutate(treatment = case_when(.$pond_id %in% c("A", "B", "C") ~ "pulsed",
-                                .$pond_id %in% c("D", "E", "F") ~ "reference")) %>%
-   relocate(treatment, .after=pond_id) %>%
-   left_join(t9 %>%
-                mutate(treatment = case_when(.$pond_id=="B" ~ "pulsed",
-                                             .$pond_id=="F" ~ "reference")) %>%
-                select(-pond_id))
+test = test %>%
+   mutate(treatment = case_when(pond_id %in% c("A", "B", "C") ~ "pulsed",
+                                pond_id %in% c("D", "E", "F") ~ "reference")) %>%
+   relocate(treatment, .after=pond_id) #%>%
+   # left_join(t9 %>%
+   #              mutate(treatment = case_when(.$pond_id=="B" ~ "pulsed",
+   #                                           .$pond_id=="F" ~ "reference")) %>%
+   #              select(-pond_id))
 
 
-model_dataset = test2 %>%
+model_dataset = test %>%
    select(pond_id, treatment, doy, period, everything()) %>%
    arrange(pond_id, doy)
 
 
-write_csv(model_dataset, file = "ghg-model-dataset_2022-01-10.csv")
+write_csv(model_dataset, file = "ghg-model-dataset_2022-04-29.csv")
+
+
+## remove temporary individual data sets
+rm(list = ls(pattern = "t[[:digit:]]"))
+##
 
