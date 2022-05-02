5e2e90a648bab585a700318ba3981062b66b63bd
Robert Johnson <johnson.robert.333@gmail.com>
2022-05-02 15:35:21 -0500

    additional model tests; testing corAR1() in models

--- Analysis-Scripts/stats_carbon-dioxide.R
+++ Analysis-Scripts/stats_carbon-dioxide.R
@@ -8,9 +8,6 @@
 if (!require(tidyverse)) install.packages('tidyverse'); library(tidyverse)
 if (!require(lme4)) install.packages('lme4'); library(lme4)
 if (!require(nlme)) install.packages('nlme'); library(nlme)
-# if (!require(lmerTest)) install.packages('lmerTest'); library('lmerTest')
-# if (!require(sjPlot)) install.packages('sjPlot'); library(sjPlot)
-# if (!require(MuMIn)) install.packages('MuMIn'); library(MuMIn)
 
 
 #-- Step 1: Prepare the data
@@ -24,11 +21,13 @@ mdat_co2 = fdat %>%
    # factor variables
    mutate(across(c(pond_id, treatment, period, period2), ~as.factor(.))) %>%
    # select desired variables
-   select(pond_id:period2, 
-          co2_flux, co2_lake, 
-          NEP, R, 
-          # tp, tn, np_ratio,
-          bottom_do, bottom_do_sat, temp, chla, alkalinity, doc_ppm) %>%
+   select(
+      pond_id:period2, 
+      co2_flux, co2_lake, 
+      R, NEP, 
+      bottom_do, bottom_do_sat, temp, chla, alkalinity, doc_ppm
+      # , tp, srp, tn, nox, np_ratio
+      ) %>%
    # only keep rows that have values for all variables
    filter(!(if_any(co2_flux:last_col(), is.na))) %>%
    # filter(!(is.na(co2_lake))) %>%
@@ -42,41 +41,82 @@ mdat_co2 = fdat %>%
 
 
 
-#-- Step 2: Build the model
+#-- Step 2: Build the full model
 
-# start with the full (most complex) model
-#  allow the intercept to vary by pond (random effect) to account for the repeated-measures sampling
+# start with the most complex (full) model
+#  allow intercept to vary by pond (random effect) to account for the repeated-measures sampling
 #  allow for temporal autocorrelation
 
-f1 = lme(co2_lake ~ treatment + period + chla + NEP + R + alkalinity + bottom_do_sat + doc_ppm + treatment:period,
+
+## Full Model
+f1 = lme(co2_lake ~ chla + NEP + R + alkalinity + bottom_do_sat + doc_ppm + treatment + period + treatment:period,
          random = ~ 1 | pond_id, data = mdat_co2, method="ML")
 
-# add AR(1) autocorrelation
+# should the model include autocorrelation (AR(1))?
 f2 = update(f1, correlation = corAR1(form = ~ time|pond_id, value = ACF(f1, form = ~ time|pond_id)[2,2]))
 
 anova(f1, f2)  # f2 is better
 
+f3 = lme(co2_lake ~ chla + NEP + R + alkalinity + bottom_do_sat + doc_ppm + treatment + period + treatment:period,
+         random = ~ 1 | pond_id, correlation = corAR1(), data = mdat_co2, method="ML")
 
-# is the random effect for repeated measures significant?
-g1 = gls(co2_lake ~ treatment + period + chla + NEP + R + alkalinity + bottom_do_sat + treatment:period,
+f4 = update(f1, correlation = corAR1())
+
+f5 = update(f4, .~. - alkalinity)
+
+f6 = update(f1, .~. - alkalinity, correlation = corAR1())
+
+f7 = update(f5, correlation = corAR1())
+
+## Is 'period2' better than 'period'? (period2 also encompasses treatment, so remove both period and treatment variables)
+m1 = update(f1, .~. - treatment - period - treatment:period + period2)
+m2 = update(m1, correlation = corAR1(form = ~ time|pond_id, value = ACF(m1, form = ~ time|pond_id)[2,2]))
+m3 = update(m1, correlation = corAR1())
+
+anova(m1, f2)  # f2 is better
+
+
+
+#-- Step 3: Iteratively remove components and find best-fit model
+
+# Is the random effect significant (effect of repeated measures)?
+g1 = gls(co2_lake ~ chla + NEP + R + alkalinity + bottom_do_sat + doc_ppm + treatment + period + treatment:period,
          data = mdat_co2, method="ML")
 g2 = update(g1, correlation = corAR1(form = ~ time|pond_id, value = ACF(g1, form = ~ time|pond_id)[2,2]))
 
+anova(g1, g2)  # g2 is better
 anova(g2, f2)  # f2 is better
 
 
-# is 'period2' better than 'period'? (period2 also encompasses treatment, so remove both period and treatment variables)
-m1 = update(f1, .~. - period - treatment - treatment:period + period2)
-m2 = update(m1, correlation = corAR1(form = ~ time|pond_id, value = ACF(m1, form = ~ time|pond_id)[2,2]))
 
-anova(m2, f2)  # f2 is better
-
-
-# pulsed ponds only, what is significant when only using treatment ponds, viewing effects of the pulses?
+## Pulsed ponds only - are the pulsed "periods" different from the pre-pulse (base) period?
 p1 = update(f1, .~. - treatment - treatment:period + alkalinity:period, data = mdat_co2 %>% filter(treatment=="pulsed"), method="REML")
 p2 = update(p1, correlation = corAR1(form = ~ time|pond_id, value = ACF(p1, form = ~ time|pond_id)[2,2]))
 
 summary(p2)
+
+
+## use nutrient concentration instead of treatment to represent differences
+# t1 = lme(co2_lake ~ tp + tn + NEP + R + chla + alkalinity + bottom_do_sat + doc_ppm,
+#          random = ~1|pond_id, data = mdat_co2, method="REML")
+# t1 = update(t1, correlation = corAR1(form = ~ time|pond_id, value = ACF(t1, form = ~ time|pond_id)[2,2]))
+# 
+# summary(t1)
+
+
+## Compare only post-pulse time periods
+
+m2 = update(f1, data = mdat_co2 %>% filter(period!="BASE"), method='ML')
+m2 = update(m2, correlation = corAR1(form = ~ time|pond_id, value = ACF(m2, form = ~ time|pond_id)[2,2]))
+
+# anova(m2, f2)  # erroneous; cannot compare models with different n()
+
+# without including pulse-period as an effect
+m3 = update(f1, .~. - period - treatment:period, data = mdat_co2 %>% filter(period!="BASE"), method='ML')
+m3 = update(m3, correlation = corAR1(form = ~ time|pond_id, value = ACF(m3, form = ~ time|pond_id)[2,2]))
+
+anova(m2, m3)  # m2 is better
+
 
 
 
