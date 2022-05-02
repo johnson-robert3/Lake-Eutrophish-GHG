18a01de74bb158a417e9a0f6fd2b999827c23fe3
Robert Johnson <johnson.robert.333@gmail.com>
2022-05-01 20:57:25 -0500

    added full and init. models; moved fdat changes to model-data script

--- Analysis-Scripts/stats_carbon-dioxide.R
+++ Analysis-Scripts/stats_carbon-dioxide.R
@@ -16,27 +16,22 @@ if (!require(nlme)) install.packages('nlme'); library(nlme)
 #-- Step 1: Prepare the data
 
 # Full data set
-fdat = read_csv("Data/ghg-model-dataset_2022-04-30.csv")
-
-# changes relevant to all models
-fdat = fdat %>%
-   mutate(np_ratio = (tn/14.001) / ((tp/1000)/30.97),
-          doc_ppm = doc_ppb / 1000) %>%
-   # add a new 'period' variable to account for the fact that pulse periods don't mean anything for the reference treatment
-   #  reference ponds are, in effect, always in the 'Base' period
-   mutate(period2 = if_else(treatment=="reference", "BASE", period)) %>%
-   relocate(period2, .after=period) %>%
-   # factor variables
-   mutate(across(c(pond_id, treatment, period, period2), ~as.factor(.)))
+fdat = read_csv("Data/ghg-model-dataset_2022-05-01.csv")
 
 
 # Data for the CO2 model
 mdat_co2 = fdat %>%
+   # factor variables
+   mutate(across(c(pond_id, treatment, period, period2), ~as.factor(.))) %>%
    # select desired variables
-   select(pond_id:period, co2_lake, NEP, np_ratio, tp, srp, tn, nox, bottom_do_sat, temp, chla) %>%
+   select(pond_id:period2, 
+          co2_flux, co2_lake, 
+          NEP, R, 
+          # tp, tn, np_ratio,
+          bottom_do, bottom_do_sat, temp, chla, alkalinity, doc_ppm) %>%
    # only keep rows that have values for all variables
-   # filter(!(if_any(co2_lake:last_col(), is.na))) %>%
-   filter(!(is.na(co2_lake))) %>%
+   filter(!(if_any(co2_flux:last_col(), is.na))) %>%
+   # filter(!(is.na(co2_lake))) %>%
    # add a "time" variable for autocorrelation
    group_by(pond_id) %>%
    arrange(doy, .by_group=TRUE) %>%
@@ -49,8 +44,39 @@ mdat_co2 = fdat %>%
 
 #-- Step 2: Build the model
 
-# start with the full model
+# start with the full (most complex) model
 #  allow the intercept to vary by pond (random effect) to account for the repeated-measures sampling
 #  allow for temporal autocorrelation
+
+f1 = lme(co2_lake ~ treatment + period + chla + NEP + R + alkalinity + bottom_do_sat + doc_ppm + treatment:period,
+         random = ~ 1 | pond_id, data = mdat_co2, method="ML")
+
+# add AR(1) autocorrelation
+f2 = update(f1, correlation = corAR1(form = ~ time|pond_id, value = ACF(f1, form = ~ time|pond_id)[2,2]))
+
+anova(f1, f2)  # f2 is better
+
+
+# is the random effect for repeated measures significant?
+g1 = gls(co2_lake ~ treatment + period + chla + NEP + R + alkalinity + bottom_do_sat + treatment:period,
+         data = mdat_co2, method="ML")
+g2 = update(g1, correlation = corAR1(form = ~ time|pond_id, value = ACF(g1, form = ~ time|pond_id)[2,2]))
+
+anova(g2, f2)  # f2 is better
+
+
+# is 'period2' better than 'period'? (period2 also encompasses treatment, so remove both period and treatment variables)
+m1 = update(f1, .~. - period - treatment - treatment:period + period2)
+m2 = update(m1, correlation = corAR1(form = ~ time|pond_id, value = ACF(m1, form = ~ time|pond_id)[2,2]))
+
+anova(m2, f2)  # f2 is better
+
+
+# pulsed ponds only, what is significant when only using treatment ponds, viewing effects of the pulses?
+p1 = update(f1, .~. - treatment - treatment:period + alkalinity:period, data = mdat_co2 %>% filter(treatment=="pulsed"), method="REML")
+p2 = update(p1, correlation = corAR1(form = ~ time|pond_id, value = ACF(p1, form = ~ time|pond_id)[2,2]))
+
+summary(p2)
+
 
 
