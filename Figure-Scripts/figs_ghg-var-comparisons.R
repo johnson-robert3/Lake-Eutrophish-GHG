65b31f8d5b03cbb2f0383c2f955282161c629ec3
Robert Johnson <johnson.robert.333@gmail.com>
2022-05-05 20:46:49 -0500

    added N2O fig

--- Figure-Scripts/figs_ghg-var-comparisons.R
+++ Figure-Scripts/figs_ghg-var-comparisons.R
@@ -14,7 +14,7 @@ source("Figure-Scripts/figs_functions.R")
 
 
 #===
-#### CO2 ####
+#### CO2 - Carbon Dioxide ####
 #===
 
 # CO2 over time
@@ -38,13 +38,13 @@ ggplot(mdat_co2 %>%
    #
    # ggtitle(expression(CO[2])) +
    theme_classic() +
-   theme(legend.position = c(0.18, 0.85),
+   theme(legend.position = c(0.18, 0.9),
          axis.title.y = element_text(margin = margin(r=0.5, unit="line")))
 
 
 
 #===
-#### CH4 ####
+#### CH4 - Methane ####
 #===
 
 # CH4 over time
@@ -60,7 +60,7 @@ ggplot(mdat_ch4 %>%
    geom_line(aes(color = trt_nutrients, group = pond_id), alpha=0.3, size=1) +
    # geom_point(aes(color = trt_nutrients), size=1.5, alpha=0.4) +
    # treatment mean (loess smooth)
-   # geom_smooth(aes(color = trt_nutrients), size=1.5, alpha=0.8, se=F, span=0.15) +
+   geom_smooth(aes(color = trt_nutrients), size=1.5, alpha=0.8, se=F, span=0.15) +
    #
    scale_color_manual(name = NULL, breaks = nut_breaks, values = nut_color, labels = nut_labs) +
    scale_x_continuous(name = " ", limits = c(140, 245), breaks = seq(140,240,20)) +
@@ -68,7 +68,40 @@ ggplot(mdat_ch4 %>%
    #
    # ggtitle(expression(CH[4])) +
    theme_classic() +
-   theme(legend.position = c(0.18, 0.85),
+   theme(legend.position = c(0.8, 0.9),
          axis.title.y = element_text(margin = margin(r=0.5, unit="line")))
+
+
+
+#===
+#### N2O - Nitrous Oxide ####
+#===
+
+# N2O over time
+
+windows(height=4, width=5)
+ggplot(mdat_n2o %>%
+          # n2o concentration data already in nM from model dataset
+          left_join(pond_data),
+       aes(x = doy, y = n2o_lake)) +
+   #
+   geom_hline(yintercept=0, linetype=3, color="gray60") +
+   geom_vline(xintercept = c(176, 211), linetype=2, color="gray60") +
+   # pond data
+   geom_line(aes(color = trt_nutrients, group = pond_id), alpha=0.3, size=1) +
+   # geom_point(aes(color = trt_nutrients), size=1.5, alpha=0.4) +
+   # treatment mean (loess smooth)
+   geom_smooth(aes(color = trt_nutrients), size=1.5, alpha=0.8, se=F, span=0.15) +
+   #
+   scale_color_manual(name = NULL, breaks = nut_breaks, values = nut_color, labels = nut_labs) +
+   scale_x_continuous(name = " ", limits = c(140, 245), breaks = seq(140,240,20)) +
+   scale_y_continuous(name = expression(Dissolved~N[2]*O~(nM))) +
+   #
+   # ggtitle(expression(N[2]*O)) +
+   theme_classic() +
+   theme(legend.position = c(0.8, 0.9),
+         axis.title.y = element_text(margin = margin(r=0.5, unit="line")))
+
+
 
 
