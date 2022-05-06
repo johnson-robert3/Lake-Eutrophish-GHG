999dff70df9e3169e146dca77428674a67feaf48
Robert Johnson <johnson.robert.333@gmail.com>
2022-05-06 11:19:03 -0500

    added CH4 and N2O vs model variables

--- Figure-Scripts/figs_ghg-var-comparisons.R
+++ Figure-Scripts/figs_ghg-var-comparisons.R
@@ -13,6 +13,13 @@ library(slider)
 source("Figure-Scripts/figs_functions.R")
 
 
+# Pond/Site Data
+pond_data = read_csv("Data/R-Data/2020_pond-data.csv")
+
+# Data for plotting
+pdat = left_join(mdat, pond_data %>% select(pond_id, starts_with("trt")))
+
+
 #===
 #### CO2 - Carbon Dioxide ####
 #===
@@ -20,9 +27,7 @@ source("Figure-Scripts/figs_functions.R")
 # CO2 over time
 
 windows(height=4, width=5)
-ggplot(mdat_co2 %>%
-          left_join(pond_data),
-       aes(x = doy, y = co2_lake)) +
+ggplot(pdat, aes(x = doy, y = co2_lake)) +
    #
    geom_hline(yintercept=0, linetype=3, color="gray60") +
    geom_vline(xintercept = c(176, 211), linetype=2, color="gray60") +
@@ -47,12 +52,9 @@ ggplot(mdat_co2 %>%
 #### CH4 - Methane ####
 #===
 
-# CH4 over time
-
+## CH4 over time
 windows(height=4, width=5)
-ggplot(mdat_ch4 %>%
-          left_join(pond_data),
-       aes(x = doy, y = ch4_lake)) +
+ggplot(pdat, aes(x = doy, y = ch4_lake)) +
    #
    geom_hline(yintercept=0, linetype=3, color="gray60") +
    geom_vline(xintercept = c(176, 211), linetype=2, color="gray60") +
@@ -72,18 +74,159 @@ ggplot(mdat_ch4 %>%
          axis.title.y = element_text(margin = margin(r=0.5, unit="line")))
 
 
+## CH4 vs SRP
+windows(height=4, width=5)
+ggplot(pdat, aes(x = srp, y = ch4_lake)) +
+   #
+   geom_point(aes(color = trt_nutrients), alpha=0.8, size=1.5) +
+   #
+   scale_color_manual(name = NULL, breaks = nut_breaks, values = nut_color, labels = nut_labs) +
+   scale_x_continuous(name = "SRP") +
+   scale_y_continuous(name = expression(Dissolved~CH[4]~(mu*M))) +
+   # 
+   theme_classic()
+
+# treatment means
+windows(height=4, width=5)
+ggplot(pdat %>% 
+          group_by(trt_nutrients, doy) %>%
+          summarize(across(ch4_lake:np_ratio, list(mean=mean, sd=sd), .names="{.fn}_{.col}")) %>%
+          ungroup(), 
+       aes(x = mean_srp, y = mean_ch4_lake)) +
+   #
+   geom_point(aes(color = trt_nutrients), alpha=0.8, size=2) +
+   #
+   scale_color_manual(name = NULL, breaks = nut_breaks, values = nut_color, labels = nut_labs) +
+   scale_x_continuous(name = "SRP") +
+   scale_y_continuous(name = expression(Dissolved~CH[4]~(mu*M))) +
+   # 
+   theme_classic()
+
+
+## CH4 vs Bottom DO
+windows(height=4, width=5)
+ggplot(pdat, aes(x = bottom_do, y = ch4_lake)) +
+   #
+   geom_point(aes(color = trt_nutrients), alpha=0.8, size=1.5) +
+   #
+   scale_color_manual(name = NULL, breaks = nut_breaks, values = nut_color, labels = nut_labs) +
+   scale_x_continuous(name = "DO in bottom 20cm") +
+   scale_y_continuous(name = expression(Dissolved~CH[4]~(mu*M))) +
+   # 
+   theme_classic()
+
+# treatment means
+windows(height=4, width=5)
+ggplot(pdat %>% 
+          group_by(trt_nutrients, doy) %>%
+          summarize(across(ch4_lake:np_ratio, list(mean=mean, sd=sd), .names="{.fn}_{.col}")) %>%
+          ungroup(), 
+       aes(x = mean_bottom_do, y = mean_ch4_lake)) +
+   #
+   geom_point(aes(color = trt_nutrients), alpha=0.8, size=2) +
+   #
+   scale_color_manual(name = NULL, breaks = nut_breaks, values = nut_color, labels = nut_labs) +
+   scale_x_continuous(name = "DO in bottom 20cm") +
+   scale_y_continuous(name = expression(Dissolved~CH[4]~(mu*M))) +
+   # 
+   theme_classic()
+
+
+## CH4 vs R
+windows(height=4, width=5)
+ggplot(pdat, aes(x = R, y = ch4_lake)) +
+   #
+   geom_point(aes(color = trt_nutrients), alpha=0.8, size=1.5) +
+   #
+   scale_color_manual(name = NULL, breaks = nut_breaks, values = nut_color, labels = nut_labs) +
+   scale_x_continuous(name = "Respiration") +
+   scale_y_continuous(name = expression(Dissolved~CH[4]~(mu*M))) +
+   # 
+   theme_classic()
+
+# treatment means
+windows(height=4, width=5)
+ggplot(pdat %>% 
+          group_by(trt_nutrients, doy) %>%
+          summarize(across(ch4_lake:np_ratio, list(mean=mean, sd=sd), .names="{.fn}_{.col}")) %>%
+          ungroup(), 
+       aes(x = mean_R, y = mean_ch4_lake)) +
+   #
+   geom_point(aes(color = trt_nutrients), alpha=0.8, size=2) +
+   #
+   scale_color_manual(name = NULL, breaks = nut_breaks, values = nut_color, labels = nut_labs) +
+   scale_x_continuous(name = "Respiration") +
+   scale_y_continuous(name = expression(Dissolved~CH[4]~(mu*M))) +
+   # 
+   theme_classic()
+
+
+## CH4 vs Chla
+windows(height=4, width=5)
+ggplot(pdat, aes(x = chla, y = ch4_lake)) +
+   #
+   geom_point(aes(color = trt_nutrients), alpha=0.8, size=1.5) +
+   #
+   scale_color_manual(name = NULL, breaks = nut_breaks, values = nut_color, labels = nut_labs) +
+   scale_x_continuous(name = "Surface Chla") +
+   scale_y_continuous(name = expression(Dissolved~CH[4]~(mu*M))) +
+   # 
+   theme_classic()
+
+# treatment means
+windows(height=4, width=5)
+ggplot(pdat %>% 
+          group_by(trt_nutrients, doy) %>%
+          summarize(across(ch4_lake:np_ratio, list(mean=mean, sd=sd), .names="{.fn}_{.col}")) %>%
+          ungroup(), 
+       aes(x = mean_chla, y = mean_ch4_lake)) +
+   #
+   geom_point(aes(color = trt_nutrients), alpha=0.8, size=2) +
+   #
+   scale_color_manual(name = NULL, breaks = nut_breaks, values = nut_color, labels = nut_labs) +
+   scale_x_continuous(name = "Surface Chla") +
+   scale_y_continuous(name = expression(Dissolved~CH[4]~(mu*M))) +
+   # 
+   theme_classic()
+
+
+## CH4 vs DOC
+windows(height=4, width=5)
+ggplot(pdat, aes(x = doc_ppm, y = ch4_lake)) +
+   #
+   geom_point(aes(color = trt_nutrients), alpha=0.8, size=1.5) +
+   #
+   scale_color_manual(name = NULL, breaks = nut_breaks, values = nut_color, labels = nut_labs) +
+   scale_x_continuous(name = "DOC") +
+   scale_y_continuous(name = expression(Dissolved~CH[4]~(mu*M))) +
+   # 
+   theme_classic()
+
+# treatment means
+windows(height=4, width=5)
+ggplot(pdat %>% 
+          group_by(trt_nutrients, doy) %>%
+          summarize(across(ch4_lake:np_ratio, list(mean=mean, sd=sd), .names="{.fn}_{.col}")) %>%
+          ungroup(), 
+       aes(x = mean_doc_ppm, y = mean_ch4_lake)) +
+   #
+   geom_point(aes(color = trt_nutrients), alpha=0.8, size=2) +
+   #
+   scale_color_manual(name = NULL, breaks = nut_breaks, values = nut_color, labels = nut_labs) +
+   scale_x_continuous(name = "DOC") +
+   scale_y_continuous(name = expression(Dissolved~CH[4]~(mu*M))) +
+   # 
+   theme_classic()
+
+
 
 #===
 #### N2O - Nitrous Oxide ####
 #===
 
-# N2O over time
-
+## N2O over time
 windows(height=4, width=5)
-ggplot(mdat_n2o %>%
-          # n2o concentration data already in nM from model dataset
-          left_join(pond_data),
-       aes(x = doy, y = n2o_lake)) +
+ggplot(pdat, aes(x = doy, y = n2o_lake)) +
    #
    geom_hline(yintercept=0, linetype=3, color="gray60") +
    geom_vline(xintercept = c(176, 211), linetype=2, color="gray60") +
@@ -103,5 +246,148 @@ ggplot(mdat_n2o %>%
          axis.title.y = element_text(margin = margin(r=0.5, unit="line")))
 
 
+## N2O vs NEP
+windows(height=4, width=5)
+ggplot(pdat, aes(x = NEP, y = n2o_lake)) +
+   #
+   geom_point(aes(color = trt_nutrients), alpha=0.8, size=1.5) +
+   #
+   scale_color_manual(name = NULL, breaks = nut_breaks, values = nut_color, labels = nut_labs) +
+   scale_x_continuous(name = "NEP") +
+   scale_y_continuous(name = expression(Dissolved~N[2]*O~(nM))) +
+   # 
+   theme_classic()
+
+# treatment means
+windows(height=4, width=5)
+ggplot(pdat %>% 
+          group_by(trt_nutrients, doy) %>%
+          summarize(across(ch4_lake:np_ratio, list(mean=mean, sd=sd), .names="{.fn}_{.col}")) %>%
+          ungroup(),
+       aes(x = mean_NEP, y = mean_n2o_lake)) +
+   #
+   geom_point(aes(color = trt_nutrients), alpha=0.8, size=2) +
+   #
+   scale_color_manual(name = NULL, breaks = nut_breaks, values = nut_color, labels = nut_labs) +
+   scale_x_continuous(name = "NEP") +
+   scale_y_continuous(name = expression(Dissolved~N[2]*O~(nM))) +
+   # 
+   theme_classic()
+
+
+## N2O vs R
+windows(height=4, width=5)
+ggplot(pdat, aes(x = R, y = n2o_lake)) +
+   #
+   geom_point(aes(color = trt_nutrients), alpha=0.8, size=1.5) +
+   #
+   scale_color_manual(name = NULL, breaks = nut_breaks, values = nut_color, labels = nut_labs) +
+   scale_x_continuous(name = "Respiration") +
+   scale_y_continuous(name = expression(Dissolved~N[2]*O~(nM))) +
+   # 
+   theme_classic()
+
+# treatment means
+windows(height=4, width=5)
+ggplot(pdat %>% 
+          group_by(trt_nutrients, doy) %>%
+          summarize(across(ch4_lake:np_ratio, list(mean=mean, sd=sd), .names="{.fn}_{.col}")) %>%
+          ungroup(),
+       aes(x = mean_R, y = mean_n2o_lake)) +
+   #
+   geom_point(aes(color = trt_nutrients), alpha=0.8, size=2) +
+   #
+   scale_color_manual(name = NULL, breaks = nut_breaks, values = nut_color, labels = nut_labs) +
+   scale_x_continuous(name = "Respiration") +
+   scale_y_continuous(name = expression(Dissolved~N[2]*O~(nM))) +
+   # 
+   theme_classic()
+
+
+## N2O vs Bottom DO
+windows(height=4, width=5)
+ggplot(pdat, aes(x = bottom_do, y = n2o_lake)) +
+   #
+   geom_point(aes(color = trt_nutrients), alpha=0.8, size=1.5) +
+   #
+   scale_color_manual(name = NULL, breaks = nut_breaks, values = nut_color, labels = nut_labs) +
+   scale_x_continuous(name = "DO in bottom 20cm") +
+   scale_y_continuous(name = expression(Dissolved~N[2]*O~(nM))) +
+   # 
+   theme_classic()
+
+# treatment means
+windows(height=4, width=5)
+ggplot(pdat %>% 
+          group_by(trt_nutrients, doy) %>%
+          summarize(across(ch4_lake:np_ratio, list(mean=mean, sd=sd), .names="{.fn}_{.col}")) %>%
+          ungroup(), 
+       aes(x = mean_bottom_do, y = mean_n2o_lake)) +
+   #
+   geom_point(aes(color = trt_nutrients), alpha=0.8, size=2) +
+   #
+   scale_color_manual(name = NULL, breaks = nut_breaks, values = nut_color, labels = nut_labs) +
+   scale_x_continuous(name = "DO in bottom 20cm") +
+   scale_y_continuous(name = expression(Dissolved~N[2]*O~(nM))) +
+   # 
+   theme_classic()
+
+
+## N2O vs TN
+windows(height=4, width=5)
+ggplot(pdat, aes(x = tn, y = n2o_lake)) +
+   #
+   geom_point(aes(color = trt_nutrients), alpha=0.8, size=1.5) +
+   #
+   scale_color_manual(name = NULL, breaks = nut_breaks, values = nut_color, labels = nut_labs) +
+   scale_x_continuous(name = "TN") +
+   scale_y_continuous(name = expression(Dissolved~N[2]*O~(nM))) +
+   # 
+   theme_classic()
+
+# treatment means
+windows(height=4, width=5)
+ggplot(pdat %>% 
+          group_by(trt_nutrients, doy) %>%
+          summarize(across(ch4_lake:np_ratio, list(mean=mean, sd=sd), .names="{.fn}_{.col}")) %>%
+          ungroup(), 
+       aes(x = mean_tn, y = mean_n2o_lake)) +
+   #
+   geom_point(aes(color = trt_nutrients), alpha=0.8, size=2) +
+   #
+   scale_color_manual(name = NULL, breaks = nut_breaks, values = nut_color, labels = nut_labs) +
+   scale_x_continuous(name = "TN") +
+   scale_y_continuous(name = expression(Dissolved~N[2]*O~(nM))) +
+   # 
+   theme_classic()
+
+
+## N2O vs NOx
+windows(height=4, width=5)
+ggplot(pdat, aes(x = nox, y = n2o_lake)) +
+   #
+   geom_point(aes(color = trt_nutrients), alpha=0.8, size=1.5) +
+   #
+   scale_color_manual(name = NULL, breaks = nut_breaks, values = nut_color, labels = nut_labs) +
+   scale_x_continuous(name = "NOx") +
+   scale_y_continuous(name = expression(Dissolved~N[2]*O~(nM))) +
+   # 
+   theme_classic()
+
+# treatment means
+windows(height=4, width=5)
+ggplot(pdat %>% 
+          group_by(trt_nutrients, doy) %>%
+          summarize(across(ch4_lake:np_ratio, list(mean=mean, sd=sd), .names="{.fn}_{.col}")) %>%
+          ungroup(), 
+       aes(x = mean_nox, y = mean_n2o_lake)) +
+   #
+   geom_point(aes(color = trt_nutrients), alpha=0.8, size=2) +
+   #
+   scale_color_manual(name = NULL, breaks = nut_breaks, values = nut_color, labels = nut_labs) +
+   scale_x_continuous(name = "NOx") +
+   scale_y_continuous(name = expression(Dissolved~N[2]*O~(nM))) +
+   # 
+   theme_classic()
 
 
