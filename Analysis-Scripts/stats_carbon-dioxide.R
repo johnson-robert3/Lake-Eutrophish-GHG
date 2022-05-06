e861aed98d3202ef0eee0ac4dd7bf3116d0bbb8e
Robert Johnson <johnson.robert.333@gmail.com>
2022-05-06 12:38:19 -0500

    move model df creation out to new script

--- Analysis-Scripts/stats_carbon-dioxide.R
+++ Analysis-Scripts/stats_carbon-dioxide.R
@@ -14,33 +14,7 @@ if (!require(nlme)) install.packages('nlme'); library(nlme)
 
 #-- Step 1: Prepare the data
 
-# Full data set
-fdat = read_csv("Data/ghg-model-dataset_2022-05-05.csv")
-
-
-# Data for GHG models
-mdat = fdat %>%
-   # factor variables
-   mutate(across(c(pond_id, treatment, period, period2), ~as.factor(.))) %>%
-   # convert N2O flux and concentration data from units of micro-mole to nano-mole
-   mutate(across(c(n2o_flux, n2o_lake), ~.*1000)) %>%
-   # select desired variables
-   select(pond_id:period2, 
-          contains("_lake"), contains("_flux"),
-          R, NEP, bottom_do, bottom_do_sat, 
-          temp, chla, alkalinity, doc_ppm, 
-          tp, srp, tn, nox, np_ratio) %>%
-   # only keep rows that have values for all variables
-   filter(!(if_any(where(is.numeric), is.na))) %>%
-   # add a "time" variable for autocorrelation
-   group_by(pond_id) %>%
-   arrange(doy, .by_group=TRUE) %>%
-   mutate(time = seq_len(n())) %>%
-   ungroup() %>%
-   # force the reference treatment to be first (so "treatment" shows the effect of the pulsed treatment in model results)
-   # mutate(treatment = fct_relevel(treatment, "reference"))
-   # force the pulsed treatment for be first (so model results show effect of continuous variables for the pulsed treatment)
-   mutate(treatment = fct_relevel(treatment, "pulsed"))
+# Run the 'stats_model-data.R' script to create the 'mdat' dataset
 
 
 
