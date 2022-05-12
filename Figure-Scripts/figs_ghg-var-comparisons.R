ab2e19bf7ad6235408cad594a3fcf82115552545
Robert Johnson <johnson.robert.333@gmail.com>
2022-05-12 13:42:04 -0500

    change flux figs x-axes from doy to date

--- Figure-Scripts/figs_ghg-var-comparisons.R
+++ Figure-Scripts/figs_ghg-var-comparisons.R
@@ -9,6 +9,7 @@ library(tidyverse)
 library(cowplot)
 library(patchwork)
 library(slider)
+library(lubridate)
 
 source("Figure-Scripts/figs_functions.R")
 
@@ -19,6 +20,9 @@ pond_data = read_csv("Data/R-Data/2020_pond-data.csv")
 # Data for plotting
 #  Need to run "stats_model-data.R" script first
 pdat = left_join(mdat, pond_data %>% select(pond_id, starts_with("trt")))
+
+# correct dates for fdat
+fdat = fdat %>% mutate(date = ymd(date))
 
 
 #===
@@ -58,18 +62,23 @@ windows(height=4, width=5.5)
 ggplot(fdat %>%
           filter(!(is.na(ch4_flux))) %>%
           left_join(pond_data), 
-       aes(x = doy, y = ch4_flux)) +
+       aes(x = date, y = ch4_flux)) +
    #
    geom_hline(yintercept=0, linetype=3, color="gray60") +
-   geom_vline(xintercept = c(176, 211), linetype=2, color="gray60") +
+   # geom_vline(xintercept = c(176, 211), linetype=2, color="gray60") +
+   geom_vline(data = ~filter(.x, doy %in% c(176, 211)), 
+              aes(xintercept = date), linetype=2, color="gray60") +
    # pond data
    geom_line(aes(color = trt_nutrients, group = pond_id), alpha=0.3, size=1) +
-   # geom_point(aes(color = trt_nutrients), size=1.5, alpha=0.4) +
    # treatment mean (loess smooth)
    geom_smooth(aes(color = trt_nutrients), size=1.5, alpha=0.8, se=F, span=0.15) +
    #
    scale_color_manual(name = NULL, breaks = nut_breaks, values = nut_color, labels = nut_labs) +
-   scale_x_continuous(name = "Day of year", limits = c(140, 245), breaks = seq(140,240,20)) +
+   # scale_x_continuous(name = "Day of year", limits = c(140, 245), breaks = seq(140,240,20)) +
+   scale_x_date(name = NULL, 
+                breaks = as_date(c('2020-06-01', '2020-06-15', '2020-07-01', '2020-07-15', '2020-08-01', '2020-08-15', '2020-09-01')), 
+                # date_labels = "%b %Y",
+                labels = c('Jun 1', '', 'Jul 1', '', 'Aug 1', '', " ")) + 
    scale_y_continuous(name = expression(CH[4]~flux~(mmol~m^-2~d^-1)),
                       limits = c(0, 60), breaks = seq(0, 60, 10)) +
    #
@@ -77,8 +86,11 @@ ggplot(fdat %>%
    theme_classic() +
    theme(panel.border = element_rect(fill=NA, color='black'),
          legend.position = c(0.82, 0.88),
-         axis.title.x = element_text(margin = margin(t=0.5, unit="line")),
-         axis.title.y = element_text(margin = margin(r=0.5, unit="line")))
+         axis.ticks.length = unit(0.3, 'line'),
+         axis.text = element_text(color='black', size=rel(1)),
+         axis.text.x = element_text(hjust=0.2, margin = margin(t=0.5, unit='line')),
+         # axis.title.x = element_text(margin = margin(t=0.5, unit="line")),
+         axis.title.y = element_text(margin = margin(r=0.5, unit="line"), size=rel(1.1)))
 
 # ggsave(filename="diffusive-ch4-flux.png", height=4, width=5.5, units='in')
 
@@ -324,17 +336,23 @@ ggplot(fdat %>%
           filter(!(is.na(n2o_flux))) %>%
           mutate(n2o_flux = n2o_flux * 1000) %>%
           left_join(pond_data), 
-       aes(x = doy, y = n2o_flux)) +
+       aes(x = date, y = n2o_flux)) +
    #
    geom_hline(yintercept=0, linetype=3, color="gray60") +
-   geom_vline(xintercept = c(176, 211), linetype=2, color="gray60") +
+   # geom_vline(xintercept = c(176, 211), linetype=2, color="gray60") +
+   geom_vline(data = ~filter(.x, doy %in% c(176, 211)), 
+              aes(xintercept = date), linetype=2, color="gray60") +
    # pond data
    geom_line(aes(color = trt_nutrients, group = pond_id), alpha=0.3, size=1) +
    # treatment mean (loess smooth)
    geom_smooth(aes(color = trt_nutrients), size=1.5, alpha=0.8, se=F, span=0.15) +
    #
    scale_color_manual(name = NULL, breaks = nut_breaks, values = nut_color, labels = nut_labs) +
-   scale_x_continuous(name = "Day of year", limits = c(140, 245), breaks = seq(140,240,20)) +
+   # scale_x_continuous(name = "Day of year", limits = c(140, 245), breaks = seq(140,240,20)) +
+   scale_x_date(name = NULL, 
+                breaks = as_date(c('2020-06-01', '2020-06-15', '2020-07-01', '2020-07-15', '2020-08-01', '2020-08-15', '2020-09-01')),
+                # date_labels = "%b %Y",
+                labels = c('Jun 1', '', 'Jul 1', '', 'Aug 1', '', " ")) + 
    scale_y_continuous(name = expression(N[2]*O~flux~(mu*mol~m^-2~d^-1)),
                       limits = c(-4, 3), breaks = seq(-4, 3, 1)) +
    #
@@ -342,7 +360,11 @@ ggplot(fdat %>%
    theme_classic() +
    theme(panel.border = element_rect(fill=NA, color='black'),
          legend.position = c(0.51, 0.88),
-         axis.title.y = element_text(margin = margin(r=0.5, unit="line")))
+         axis.ticks.length = unit(0.3, 'line'),
+         axis.text = element_text(color='black', size=rel(1)),
+         axis.text.x = element_text(hjust=0.2, margin = margin(t=0.5, unit='line')),
+         # axis.title.x = element_text(margin = margin(t=0.5, unit="line")),
+         axis.title.y = element_text(margin = margin(r=0.5, unit="line"), size=rel(1.1)))
 
 # ggsave(filename="diffusive-n2o-flux.png", height=4, width=5.5, units='in')
 
