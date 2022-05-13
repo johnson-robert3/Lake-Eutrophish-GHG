363d1b27556c919487a0300905968815ad70a80c
Robert Johnson <johnson.robert.333@gmail.com>
2022-05-13 11:49:25 -0500

    add 1-panel NEP and R figs

--- Figure-Scripts/figs_metabolism.R
+++ Figure-Scripts/figs_metabolism.R
@@ -6,9 +6,22 @@
 
 library(cowplot)
 library(patchwork)
+library(viridis)
 library(slider)
 
 source("Figure-Scripts/figs_functions.R")
+
+
+# Pond/Site Data
+pond_data = read_csv("Data/R-Data/2020_pond-data.csv")
+
+# Data for plotting
+#  Need to run "stats_model-data.R" script first
+pdat = fdat %>% 
+   mutate(date = ymd(date)) %>%
+   left_join(pond_data %>% 
+                select(pond_id, starts_with("trt")))
+
 
 
 #--
@@ -135,6 +148,39 @@ ggsave(filename = "Figures/new-figs/roll-NEP_np-trt.png", height=7, width=6, uni
 }
 
 
+# 1 panel (blue & red)
+windows(height=4, width=5.5)
+ggplot(pdat,
+       aes(x = date, y = NEP)) +
+   #
+   geom_hline(yintercept=0, linetype=3, color="gray60") +
+   geom_vline(data = ~filter(.x, doy %in% c(176, 211)), 
+              aes(xintercept = date), linetype=2, color="gray60") +
+   # pond data
+   geom_line(aes(color = trt_nutrients, group = pond_id), alpha=0.3, size=1) +
+   # treatment mean (loess smooth)
+   geom_smooth(aes(color = trt_nutrients), size=1.5, alpha=0.8, se=F, span=0.15) +
+   #
+   scale_color_manual(name = NULL, breaks = nut_breaks, values = nut_color, labels = nut_labs) +
+   scale_x_date(name = NULL, 
+                breaks = as_date(c('2020-06-01', '2020-06-15', '2020-07-01', '2020-07-15', '2020-08-01', '2020-08-15', '2020-09-01')), 
+                labels = c('Jun 1', '', 'Jul 1', '', 'Aug 1', '', " ")) + 
+   scale_y_continuous(name = expression(NEP~(mg~O[2]~L^-1~d^-1)),
+                      limits = c(-8, 8), breaks = seq(-8, 8, 4)) +
+   ggtitle("Net Ecosystem Production") +
+   #
+   theme_classic() +
+   theme(panel.border = element_rect(fill=NA, color="black"),
+         legend.position = c(0.16, 0.16),
+         axis.ticks.length = unit(0.3, 'line'),
+         axis.text = element_text(color='black', size=rel(1)),
+         axis.text.x = element_text(hjust=0.2, margin = margin(t=0.5, unit='line')),
+         axis.title.y = element_text(margin = margin(r=0.5, unit="line"), size=rel(1.1)))
+
+# ggsave(filename = "NEP.png")
+
+
+
 #--
 # Re
 #--
@@ -254,35 +300,65 @@ ggsave(filename = "Figures/new-figs/roll-R_np-trt.png", height=7, width=6, units
 }
 
 
+# 1 panel (blue & red)
+windows(height=4, width=5.5)
+ggplot(pdat,
+       aes(x = date, y = R)) +
+   #
+   geom_hline(yintercept=0, linetype=3, color="gray60") +
+   geom_vline(data = ~filter(.x, doy %in% c(176, 211)), 
+              aes(xintercept = date), linetype=2, color="gray60") +
+   # pond data
+   geom_line(aes(color = trt_nutrients, group = pond_id), alpha=0.3, size=1) +
+   # treatment mean (loess smooth)
+   geom_smooth(aes(color = trt_nutrients), size=1.5, alpha=0.8, se=F, span=0.15) +
+   #
+   scale_color_manual(name = NULL, breaks = nut_breaks, values = nut_color, labels = nut_labs) +
+   scale_x_date(name = NULL, 
+                breaks = as_date(c('2020-06-01', '2020-06-15', '2020-07-01', '2020-07-15', '2020-08-01', '2020-08-15', '2020-09-01')), 
+                labels = c('Jun 1', '', 'Jul 1', '', 'Aug 1', '', " ")) + 
+   scale_y_continuous(name = expression(R~(mg~O[2]~L^-1~d^-1)),
+                      limits = c(-20, 0), breaks = seq(-20, 0, 5)) +
+   ggtitle("Ecosystem Respiration") +
+   #
+   theme_classic() +
+   theme(panel.border = element_rect(fill=NA, color="black"),
+         legend.position = c(0.16, 0.16),
+         axis.ticks.length = unit(0.3, 'line'),
+         axis.text = element_text(color='black', size=rel(1)),
+         axis.text.x = element_text(hjust=0.2, margin = margin(t=0.5, unit='line')),
+         axis.title.y = element_text(margin = margin(r=0.5, unit="line"), size=rel(1.1)))
+
+# ggsave(filename = "Re.png")
 
 
 
 
 # all ponds and variables
-windows(width=12)
-ggplot(metab_mle %>%
-          filter(!(GPP<0) & !(R>0)) %>%
-          pivot_longer(cols = GPP:NEP,
-                       names_to = "variable",
-                       values_to = "rate",
-                       values_drop_na = T)) +
-   geom_col(aes(x = doy, y = rate)) +
-   geom_hline(yintercept=0, linetype=2) +
-   facet_grid(rows = vars(variable), cols = vars(pond_id), scales="free_y") +
-   theme_classic()
+# windows(width=12)
+# ggplot(metab_mle %>%
+#           filter(!(GPP<0) & !(R>0)) %>%
+#           pivot_longer(cols = GPP:NEP,
+#                        names_to = "variable",
+#                        values_to = "rate",
+#                        values_drop_na = T)) +
+#    geom_col(aes(x = doy, y = rate)) +
+#    geom_hline(yintercept=0, linetype=2) +
+#    facet_grid(rows = vars(variable), cols = vars(pond_id), scales="free_y") +
+#    theme_classic()
 
 
 # just NEP
-windows(width=12)
-ggplot(metab_mle %>%
-          filter(!(GPP<0) & !(R>0)) %>%
-          pivot_longer(cols = GPP:NEP,
-                       names_to = "variable",
-                       values_to = "rate",
-                       values_drop_na = T) %>%
-          filter(variable=="NEP")) +
-   geom_col(aes(x = doy, y = rate)) +
-   geom_hline(yintercept=0, linetype=2) +
-   facet_wrap(facets = vars(pond_id), ncol=3) +
-   theme_classic()
+# windows(width=12)
+# ggplot(metab_mle %>%
+#           filter(!(GPP<0) & !(R>0)) %>%
+#           pivot_longer(cols = GPP:NEP,
+#                        names_to = "variable",
+#                        values_to = "rate",
+#                        values_drop_na = T) %>%
+#           filter(variable=="NEP")) +
+#    geom_col(aes(x = doy, y = rate)) +
+#    geom_hline(yintercept=0, linetype=2) +
+#    facet_wrap(facets = vars(pond_id), ncol=3) +
+#    theme_classic()
 
