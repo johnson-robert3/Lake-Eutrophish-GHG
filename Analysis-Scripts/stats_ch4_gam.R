821f86fd5ba033bfe0c72913dec8c37fe7722464
Robert Johnson <johnson.robert.333@gmail.com>
2022-08-02 15:00:31 -0500

    moved random forest models to separate scripts

--- Analysis-Scripts/stats_ch4_gam.R
+++ Analysis-Scripts/stats_ch4_gam.R
@@ -5,10 +5,7 @@
 #~~~
 
 
-library(party)  # conditional inference trees and random forests
 library(mgcv)   # GAMs
-
-set.seed(1987)
 
 
 # Data
@@ -21,88 +18,6 @@ gdat = mdat %>%
       -wind_speed,  # use wind_U10 instead
       -doc_ppb  # use doc_ppm
    )
-
-
-#---
-# Random Forest Models
-#---
-
-## Evaluate importance of driver variables
-
-#- Use a control forest with manually specified parameters
-
-# All ponds
-cforest_ch4 = cforest(ch4_lake ~ ., 
-                      data = gdat %>% filter(!(is.na(ch4_lake))),
-                      controls = cforest_control(ntree = 25000, mincriterion = 0.9, trace = TRUE))
-
-# identify variables of importance from the forest
-varimp_ch4 = varimp(cforest_ch4)
-
-# visualize
-windows(height=6, width=12); barplot(sort(abs(varimp_ch4), decreasing=TRUE), las=2)
-
-   ## Most important variables: 
-   # pond id
-   # bottom chla
-   # bottom phyco
-   # pulse-period
-   # do sat
-   # foodweb trt
-   # sp cond
-   # salinity
-   # percent strat prev 24-hr
-
-
-# Exclude pond A 
-cforest_ch4_noA = cforest(ch4_lake ~ ., 
-                      data = gdat %>% filter(!(is.na(ch4_lake)) & !(pond_id=='A')),
-                      controls = cforest_control(ntree = 25000, mincriterion = 0.9, trace = TRUE))
-
-# identify variables of importance from the forest
-varimp_ch4_noA = varimp(cforest_ch4_noA)
-
-# visualize
-windows(height=6, width=12); barplot(sort(abs(varimp_ch4_noA), decreasing=TRUE), las=2)
-
-   ## Most important variables: 
-   # percent strat prev 24-hr
-   # doy
-   # pulse-period
-   # do sat
-   # sp cond
-   # salinity
-   # do
-
-
-# #- Use an 'unbiased' control forest
-# 
-# # All ponds
-# cforest_ch4 = cforest(ch4_lake ~ ., 
-#                       data = gdat %>% filter(!(is.na(ch4_lake))),
-#                       controls = cforest_unbiased())
-# 
-# # identify variables of importance from the forest
-# varimp_ch4 = varimp(cforest_ch4)
-# 
-# # visualize
-# windows(height=6, width=8); barplot(sort(abs(varimp_ch4), decreasing=TRUE), las=2)
-# 
-# ## Most important variables: 
-# 
-# 
-# # Exclude pond A 
-# cforest_ch4 = cforest(ch4_lake ~ ., 
-#                       data = gdat %>% filter(!(is.na(ch4_lake)) & !(pond_id=='A')),
-#                       controls = cforest_unbiased())
-# 
-# # identify variables of importance from the forest
-# varimp_ch4 = varimp(cforest_ch4)
-# 
-# # visualize
-# windows(height=6, width=8); barplot(sort(abs(varimp_ch4), decreasing=TRUE), las=2)
-# 
-# ## Most important variables: 
 
 
 
--- Analysis-Scripts/stats_n2o_gam.R
+++ Analysis-Scripts/stats_n2o_gam.R
@@ -5,10 +5,7 @@
 #~~~
 
 
-library(party)  # conditional inference trees and random forests
 library(mgcv)   # GAMs
-
-set.seed(1987)
 
 
 # Data
@@ -21,57 +18,6 @@ gdat = mdat %>%
       -wind_speed,  # use wind_U10 instead
       -doc_ppb  # use doc_ppm
    )
-
-
-#---
-# Random Forest Models
-#---
-
-## Evaluate importance of driver variables
-
-#- Use a control forest with manually specified parameters
-
-# All ponds
-cforest_n2o = cforest(n2o_lake ~ ., 
-                      data = gdat %>% filter(!(is.na(n2o_lake))),
-                      controls = cforest_control(ntree = 25000, mincriterion = 0.9, trace = TRUE))
-
-# identify variables of importance from the forest
-varimp_n2o = varimp(cforest_n2o)
-
-# visualize
-windows(height=6, width=12); barplot(sort(abs(varimp_n2o), decreasing=TRUE), las=2)
-
-   ## Most important variables: 
-   # do
-   # doy
-   # bottom ph
-   # ph
-   # do sat
-   # bottom temp
-   # (pulse period is lower, but is the first factor variable)
-
-
-# Exclude pond A 
-cforest_n2o_noA = cforest(n2o_lake ~ ., 
-                      data = gdat %>% filter(!(is.na(n2o_lake)) & !(pond_id=='A')),
-                      controls = cforest_control(ntree = 25000, mincriterion = 0.9, trace = TRUE))
-
-# identify variables of importance from the forest
-varimp_n2o_noA = varimp(cforest_n2o_noA)
-
-# visualize
-windows(height=6, width=12); barplot(sort(abs(varimp_n2o_noA), decreasing=TRUE), las=2)
-
-   ## Most important variables: 
-   # ph
-   # bottom ph
-   # doy
-   # do
-   # do sat
-   # bottom do
-   # bottom tmep
-   # (pulse period is lower, but is the first factor variable)
 
 
 
