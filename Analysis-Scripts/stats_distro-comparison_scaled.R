cd6f37329b07a600fe4e7860330823c7472f4bee
Jonathan Walter <jawalter@ucdavis.edu>
2022-12-22 10:58:07 -0500

    scale variables in distro comparison and look at foodweb effects

---
+++ Analysis-Scripts/stats_distro-comparison_scaled.R
@@ -0,0 +1,128 @@
+## Explore hort farm pond GHG dataset from Robert Johnson & Grace Wilkinson 
+## and begin prototyping new analyses
+
+rm(list=ls())
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+dat.raw <- read.csv("../data/ghg-model-dataset_2022-07-26.csv")
+
+## basic exploration of data contents
+
+str(dat.raw)
+unique(dat.raw$pond_id)
+unique(dat.raw$doy)
+unique(dat.raw$treatment)
+
+
+## define some time periods & units of comparison/grouping
+
+wwidth <- 5 #days: width of windows, e.g. following nutrient pulses
+pulse1 <- 176: (176 + wwidth - 1)#doy
+pulse2 <- 211: (211 + wwidth - 1)
+derecho <- 220: (220 + wwidth - 1)
+heatwave <- 190:195
+
+pond_id <- unique(dat.raw$ pond_id)
+time_doy <- min(dat.raw$doy):max(dat.raw$doy)
+ref_ponds <- pond_id %in% unique(dat.raw$pond_id[dat.raw$treatment=="reference"])
+pulse_ponds <- pond_id %in% unique(dat.raw$pond_id[dat.raw$treatment=="pulsed"])
+
+
+## turn data into matrices --  mostly because Jon likes things this way
+
+
+varlist <- colnames(dat.raw)
+varlist <- varlist[!varlist %in% c("pond_id","treatment","date","doy","period","period2","week","sonde_strat","foodweb"
+                                   ,"wind_speed","wind_U10","pH","alkalinity","NOx","phyco","doc_ppb")]
+
+data_matrices <- list()
+
+for(var in varlist){
+  
+  tmp <- matrix(NA, nrow=length(pond_id), ncol=length(time_doy))
+  
+  for(ii in 1:length(pond_id)){
+    for(jj in 1:length(time_doy)){
+      
+      tmp[ii,jj] <- mean(dat.raw[,paste0(var)][dat.raw$pond_id==pond_id[ii] & dat.raw$doy==time_doy[jj]], na.rm=TRUE)
+
+      
+    }
+  }
+  
+  tmp <- t(apply(tmp, MARGIN=1, FUN=scale))
+  
+  data_matrices[[paste0(var)]] <- tmp
+}
+
+
+
+pdf("../distro_comparison_rough_scaled.pdf", onefile=TRUE)
+
+
+for(var in varlist){
+
+  tmp <- data_matrices[[paste0(var)]]
+  
+  pulsetrt.pulse1 <- tmp[pulse_ponds, time_doy %in% pulse1]
+  pulsetrt.pulse2 <- tmp[pulse_ponds, time_doy %in% pulse2]
+  heatwaveobs <- tmp[pulse_ponds, time_doy %in% heatwave]
+  derechoobs <- tmp[pulse_ponds, time_doy %in% derecho]
+  
+  ## comparison 1: pulses to other n-day windows in pulsed ponds
+  comparison1 <- NULL
+  
+  for(ii in 1:length(time_doy)){
+    
+    if(time_doy[ii]+(wwidth-1) > max(time_doy)){break} #stop when we overlap the end of the time series
+    if(time_doy[ii] %in% pulse1){next} #ignore obs in pulse 1
+    if(time_doy[ii] %in% pulse2){next} #ignore obs in pulse 2
+    
+    comparison1 <- cbind(comparison1, rowMeans(tmp[pulse_ponds, ii:(ii+wwidth-1)], na.rm=TRUE))
+    
+  }
+  
+  ecdf.ref1<- ecdf(colMeans(comparison1))
+  
+  hist(colMeans(comparison1), main="Comparison vs. other time periods", xlab=paste0(var))
+  abline(v=mean(pulsetrt.pulse1, na.rm=T), col="red")
+  abline(v=mean(pulsetrt.pulse2, na.rm=T), col="blue")
+  abline(v=mean(heatwaveobs, na.rm=T), col="magenta")
+  abline(v=mean(derechoobs, na.rm=T), col="green")
+  legend("topright", lty=1, col=c("red","blue","magenta","green"),legend=c("Pulse 1", "Pulse 2","heatwave","derecho"))
+  text(mean(pulsetrt.pulse1, na.rm=T), 0, round(ecdf.ref1(mean(pulsetrt.pulse1, na.rm=T)), 2), pos=1, col="red")
+  text(mean(pulsetrt.pulse2, na.rm=T), 0, round(ecdf.ref1(mean(pulsetrt.pulse2, na.rm=T)), 2), pos=1, col="blue")
+  
+  ## comparison 2: pulsed ponds to same time in reference ponds
+  comparison2 <- NULL
+  
+  for(ii in 1:length(time_doy)){
+    
+    if(time_doy[ii]+(wwidth-1) > max(time_doy)){break} #stop when we overlap the end of the time series
+    
+    ww <- time_doy[ii]:(time_doy[ii] + wwidth - 1)
+    if(!any(ww %in% pulse1) & !any(ww %in% pulse2)){next} #ignore windows that don't overlap pulses
+    comparison2 <- cbind(comparison2, rowMeans(tmp[ref_ponds, ii:(ii+wwidth-1)], na.rm=TRUE))
+    
+  }
+  
+  ecdf.ref2<- ecdf(colMeans(comparison2))
+  
+  hist(colMeans(comparison2), main="Comparison vs. reference ponds", xlab=paste0(var))
+  abline(v=mean(pulsetrt.pulse1, na.rm=T), col="red")
+  abline(v=mean(pulsetrt.pulse2, na.rm=T), col="blue")
+  abline(v=mean(heatwaveobs, na.rm=T), col="magenta")
+  abline(v=mean(derechoobs, na.rm=T), col="green")
+  legend("topright", lty=1, col=c("red","blue","magenta","green"),legend=c("Pulse 1", "Pulse 2","heatwave","derecho"))
+  text(mean(pulsetrt.pulse1, na.rm=T), 0, round(ecdf.ref2(mean(pulsetrt.pulse1, na.rm=T)), 2), pos=1, col="red")
+  text(mean(pulsetrt.pulse2, na.rm=T), 0, round(ecdf.ref2(mean(pulsetrt.pulse2, na.rm=T)), 2), pos=1, col="blue")
+  
+  
+}
+
+
+
+dev.off()
+
+
---
+++ Analysis-Scripts/stats_foodwebOrder.R
@@ -0,0 +1,66 @@
+## Explore hort farm pond GHG dataset from Robert Johnson & Grace Wilkinson 
+## and begin prototyping new analyses
+
+rm(list=ls())
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+dat.raw <- read.csv("../data/ghg-model-dataset_2022-07-26.csv")
+
+## basic exploration of data contents
+
+str(dat.raw)
+unique(dat.raw$pond_id)
+unique(dat.raw$doy)
+unique(dat.raw$treatment)
+dat.raw$foodweb <- NA
+dat.raw$foodweb[dat.raw$pond_id %in% c("B","F")] <- "low"
+dat.raw$foodweb[dat.raw$pond_id %in% c("A","D")] <- "mid"
+dat.raw$foodweb[dat.raw$pond_id %in% c("C","E")] <- "high"
+
+# define some time periods of interest
+wwidth=5
+pulse1 <- 176: (176 + wwidth - 1)#doy
+pulse2 <- 211: (211 + wwidth - 1)
+derecho <- 220: (220 + wwidth - 1)
+heatwave <- 190:195
+
+
+varlist <- colnames(dat.raw)
+varlist <- varlist[!varlist %in% c("pond_id","treatment","date","doy","period","period2","week","sonde_strat","foodweb"
+                                   ,"wind_speed","wind_U10","ph","alkalinity","NOx","phyco","doc_ppb")]
+
+
+cols <- c("green","yellow","red")
+
+pdf("../foodweb_consistency.pdf", onefile=TRUE)
+
+par(mfrow=c(4,2), mar=c(2.5,2.5,2.5,0.5))
+
+for(var in varlist){
+  
+  tmp <- dat.raw
+  tmp$treatment = factor(tmp$treatment, levels=c("reference","pulsed"))
+  tmp$foodweb = factor(tmp$foodweb, levels=c("low","mid","high"))
+  colnames(tmp)[colnames(tmp)==var] <- "var"
+  boxplot(var ~ treatment + foodweb, data=tmp)
+  mtext(paste0(var), line=1.1, cex=2/3)
+
+  pondmean.ref <- aggregate(dat.raw[dat.raw$treatment=="reference",var], 
+                            by=list(dat.raw[dat.raw$treatment=="reference","foodweb"]), FUN="mean", na.rm=TRUE)
+  pondmean.pulse <- aggregate(dat.raw[dat.raw$treatment=="pulsed",var], 
+                            by=list(dat.raw[dat.raw$treatment=="pulsed","foodweb"]), FUN="mean", na.rm=TRUE)
+  
+  plotmat <- cbind(rank(pondmean.ref$x), rank(pondmean.pulse$x))[c(2,3,1),]
+  image(x=1:3, y=1:2, z=plotmat, col=cols, xaxt="n", yaxt="n", xlab="", ylab="")
+  axis(1, at=1:3, labels=c("lo","int","hi"))
+  axis(2, at=1:2, labels=c("ref","pulse"))
+  mtext(paste0("Spearman correlation = ", cor(pondmean.ref$x, pondmean.pulse$x, method="spearman", 
+                                              use="pairwise.complete.obs")), line=0.1, cex=2/3)
+  mtext(paste0(var), line=1.1, cex=2/3)
+
+  
+}
+
+dev.off()
+
