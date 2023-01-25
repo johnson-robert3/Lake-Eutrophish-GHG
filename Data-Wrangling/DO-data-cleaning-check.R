ad442786b1c16df64e51d1a2e8d81d8ea33f4e6f
Robert Johnson <johnson.robert.333@gmail.com>
2023-01-25 10:28:14 -0600

    small updates, summary values, additional comments

--- Data-Wrangling/DO-data-cleaning-check.R
+++ Data-Wrangling/DO-data-cleaning-check.R
@@ -28,7 +28,7 @@ minidot %>%
              interp = sum(points_interp)) %>%
    ungroup() %>%
    mutate(perc_drop = drop / total * 100,
-          perc_interp = interp / total * 100) %>%
+          perc_interp = interp / total * 100) #%>%
    # group_by(pond_id) %>%
    # summarize(min_perc_drop = min(perc_drop),
    #           max_perc_drop = max(perc_drop),
@@ -37,7 +37,12 @@ minidot %>%
    #           max_perc_interp = max(perc_interp),
    #           mean_perc_interp = mean(perc_interp)) %>%
    # ungroup() %>%
-   View
+   # View
+
+# percent of points removed and backfilled
+summarize(do_cleaning_pts, mean(perc_interp))   # mean = 7.1 %
+summarize(do_cleaning_pts, range(perc_interp))   # range = 0 - 54.2 %
+summarize(do_cleaning_pts, median(perc_interp))   # median = 0 %
 
 
 # next: view a distribution of number of points dropped across ponds and days, are there days where too much of the data is being removed with this 
@@ -50,7 +55,7 @@ windows(); hist(do_cleaning_pts %>% filter(pond_id=="A") %>% .$drop)
 windows(); ggplot(do_cleaning_pts) +
    geom_histogram(aes(x = drop)) +
    facet_wrap(facets = vars(pond_id), nrow=3) +
-   ylab("number of flagged and dropped points") +
+   ylab("number of points flagged and dropped") +
    theme_classic()
 
 # view histograms of number of points dropped and backfilled via linear interpolation within days by pond
@@ -60,7 +65,7 @@ windows(); hist(do_cleaning_pts %>% filter(pond_id=="A") %>% .$interp)
 windows(); ggplot(do_cleaning_pts) +
    geom_histogram(aes(x = interp)) +
    facet_wrap(facets = vars(pond_id), nrow=3) +
-   ylab("number of points removed and interpolated") +
+   ylab("number of points removed and backfilled") +
    theme_classic()
 
 # time series of number of points dropped per day
@@ -74,7 +79,7 @@ windows(); ggplot(do_cleaning_pts) +
 windows(); ggplot(do_cleaning_pts) +
    geom_line(aes(x=doy, y = interp)) +
    facet_wrap(facets = vars(pond_id), nrow=3) +
-   ylab("number of points removed and interpolated") +
+   ylab("number of points removed and backfilled") +
    theme_classic()
 
 # rank plot of percentage of points removed and interpolated for each pond
@@ -94,11 +99,11 @@ windows(); ggplot(do_cleaning_pts %>%
    ylab("Percent of data points each day removed and backfilled via linear interpolation") +
    theme_classic()
 
-# how many days in each pond over thresholds of percent of points interpolated
+# how many days in each pond over a threshold percentage for number of data points removed and backfilled
 do_cleaning_pts %>% filter(perc_interp > 50) %>% count(pond_id)
 do_cleaning_pts %>% filter(perc_interp > 40) %>% count(pond_id)
-do_cleaning_pts %>% filter(perc_interp > 33) %>% count(pond_id) # n = 10
-do_cleaning_pts %>% filter(perc_interp > 25) %>% count(pond_id) # n = 20
+do_cleaning_pts %>% filter(perc_interp > 33) %>% count(pond_id) # n = 10  # 1.7% of pond-days
+do_cleaning_pts %>% filter(perc_interp > 25) %>% count(pond_id) # n = 20  # 3.5% of pond-days
 
 
 
@@ -107,34 +112,41 @@ do_cleaning_pts %>% filter(perc_interp > 25) %>% count(pond_id) # n = 20
    metabolism = read_csv("Data/metabolism_total.csv")
 
 
-# number of days per pond with erroneous metabolism estimates
+# number of days per pond with erroneous metabolism estimates (metabolism calculated w/o removing any days beforehand due to DO cleaning >33%)
 metabolism %>% filter(GPP<0 | R>0) %>% count(pond_id)
 metabolism %>% filter(GPP<0 | R>0) %>% View
+# 62 days total
+
+# percent range of days removed per pond (experiment was 96 days)
+metabolism %>% filter(GPP<0 | R>0) %>% count(pond_id) %>% mutate(perc = n / 96 * 100)
 
 
-# view number of dropped DO points on days with erroneous estimates
-metabolism %>% filter(GPP<0 | R>0) %>% left_join(test) %>% count(drop)
-metabolism %>% filter(GPP<0 | R>0) %>% left_join(test) %>% arrange(drop) %>% View
+# view number of DO points removed and backfilled on days with erroneous estimates
+metabolism %>% filter(GPP<0 | R>0) %>% left_join(do_cleaning_pts) %>% count(drop)
+metabolism %>% filter(GPP<0 | R>0) %>% left_join(do_cleaning_pts) %>% arrange(drop) %>% View
 # not as much overlap as I expected. 
-# none of the days with >25% of points removed (and interpolated) overlapped with days with erroneous metabolism estimates (perhaps b/c we "fixed" these?)
+# none of the days with >25% of points removed and backfilled overlapped with days producing erroneous metabolism estimates (perhaps b/c we "fixed" these?)
 # of 62 days with erroneous estimates, 40 were on days that did not have any DO data points dropped
 
 # 62 days with erroneous estimates using corrected DO data
 # 71 days with erroneous estimates using raw DO data
 # 16 days with >=3 flagged DO points
-# 20 days with >25% of points removed and interpolated
+# 20 days with >25% of points removed and backfilled
+# 10 days with >33% of points removed and backfilled
 
 
-# combined dataset to view metabolism around days when a higher percent of DO data points were cleaned
+# combined dataset of metabolism and DO cleaning
 test = full_join(metabolism, do_cleaning_pts) %>%
    rename(n_total = total, n_drop = drop, n_interp = interp) %>%
    mutate(flag_33 = case_when(perc_interp > 33 ~ 1,
                               TRUE ~ 0))
 
 
+# number of pond-days with >33% of DO data points removed and backfilled
 test %>% filter(flag_33 == 1) %>% count(pond_id)
 
 
+# view metabolism around days when a higher percent of DO data points were cleaned
 windows(); ggplot(test,
                   aes(x = doy, y = NEP)) +
    # all metabolism
