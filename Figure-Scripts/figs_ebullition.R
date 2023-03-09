d13c4cbc10a6b999287ceb831665a0bfb21f2bf2
Robert Johnson <johnson.robert.333@gmail.com>
2023-03-08 22:27:09 -0500

    add new time-series fig

--- Figure-Scripts/figs_ebullition.R
+++ Figure-Scripts/figs_ebullition.R
@@ -10,6 +10,8 @@ library(patchwork)
 source("Figure-Scripts/figs_functions.R")
 
 
+# dataset
+ebu_flux_pond = read_csv("Data/ebullition_total.csv")
 
 
 # bars
@@ -53,7 +55,7 @@ ggplot(ebu_flux_pond %>%
    geom_hline(yintercept=0, linetype=2) +
    geom_errorbar(aes(ymin = mean - se, ymax = mean + se), width=0, color="gray60") +
    geom_point(color = "cornflowerblue", size=3) +
-   geom_line(color = "cornflowerblue") +
+   # geom_line(color = "cornflowerblue") +
    lims(y = c(0,12)) +
    theme_classic()
 
@@ -72,7 +74,7 @@ ggplot(ebu_flux_pond %>%
    geom_hline(yintercept=0, linetype=2) +
    geom_errorbar(aes(ymin = mean - se, ymax = mean + se), width=0, color="gray60") +
    geom_point(color = "seagreen3", size=3) +
-   geom_line(color = "seagreen3") +
+   # geom_line(color = "seagreen3") +
    lims(y = c(0,12)) +
    theme_classic()
 
@@ -131,5 +133,43 @@ ggplot(ebu_flux_pond %>%
 
 windows(); r / p
 
-ggsave(filename = "Figures/new-figs/ebullition_ch4.png", height=7, width=6, units="in")
+# ggsave(filename = "Figures/new-figs/ebullition_ch4.png", height=7, width=6, units="in")
+
+
+# 1 panel as points
+
+windows(height=4, width=6); ggplot(ebu_flux_pond %>% 
+                     left_join(pond_data) %>%
+                     group_by(trt_nutrients, doy) %>%
+                     summarize(n = n(),
+                               se = sd(ch4_ebu_flux)/sqrt(n),
+                               mean = mean(ch4_ebu_flux)) %>%
+                     ungroup(),
+                  aes(x = doy, y = mean, group = trt_nutrients)) +
+   #
+   geom_hline(yintercept=0, linetype=3, color="gray60") +
+   # pulse days
+   geom_vline(xintercept = c(176, 211), linetype=1, color="gray60") +
+   # derecho, DOY 223 (Aug. 10, 2020)
+   geom_vline(xintercept = 223, linetype=1, color='gray60') +
+   # heat wave, DOY 186-190 (July 4-8, 2020)
+   annotate(geom = 'rect',
+            xmin = 186, xmax = 190,
+            ymin = -Inf, ymax = Inf,
+            fill = 'gray90') +
+   #
+   geom_point(aes(color = trt_nutrients), size=3, shape=19) +
+   geom_errorbar(aes(ymin = mean - se, ymax = mean + se), width=0, color="gray60") +
+   geom_line(aes(color = trt_nutrients), size=0.6, linetype=2) +
+   #
+   scale_y_continuous(name = expression(Ebullition)) +
+   scale_color_manual(name = NULL, breaks = nut_breaks, values = nut_color, labels = nut_labs) +
+   #
+   theme_classic()
+
+
+
+
+
+
 
