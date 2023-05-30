3e30fc7bb15f8c74c9782504c0650a414996e41e
Robert Johnson <johnson.robert.333@gmail.com>
2023-05-30 12:31:04 -0500

    add GPP figure

--- Figure-Scripts/figs_metabolism.R
+++ Figure-Scripts/figs_metabolism.R
@@ -346,6 +346,48 @@ ggplot(pdat,
 
 
 
+#--
+# GPP
+#--
+
+# 1 panel (blue & red)
+windows(height=4, width=5.5)
+ggplot(pdat,
+       aes(x = doy, y = GPP)) +
+   #
+   # geom_vline(data = ~filter(.x, doy %in% c(176, 211)), 
+   #            aes(xintercept = date), linetype=2, color="gray60") +
+   geom_vline(xintercept = c(176.5, 211.5), linetype=1, color="gray40") +
+   geom_vline(xintercept = 223, linetype=2, color="gray40") +
+   annotate(geom = 'rect', xmin = 185, xmax = 190, ymin = -Inf, ymax = Inf, fill = 'gray75') +
+   geom_hline(yintercept=0, linetype=3, color="gray60") +
+   # pond data
+   geom_line(aes(color = trt_nutrients, group = pond_id), alpha=0.4, size=0.5) +
+   # treatment mean
+   # stat_smooth(aes(color = trt_nutrients), geom="line", size=1.5, alpha=0.8, span=0.15) +
+   geom_line(data = ~.x %>% group_by(trt_nutrients, doy) %>% summarize(mean = mean(GPP, na.rm=T)) %>% ungroup(),
+             aes(x = doy, y = mean, color = trt_nutrients), size=1.3, alpha=1) +
+   #
+   scale_color_manual(name = NULL, breaks = nut_breaks, values = nut_color, labels = nut_labs) +
+   # scale_x_date(name = NULL, 
+   #              breaks = as_date(c('2020-06-01', '2020-06-15', '2020-07-01', '2020-07-15', '2020-08-01', '2020-08-15', '2020-09-01')), 
+   #              labels = c('Jun 1', '', 'Jul 1', '', 'Aug 1', '', " ")) + 
+   scale_x_continuous(name = "Day of year", limits = c(142, 242), breaks = seq(140,240,20)) +
+   scale_y_continuous(name = expression(GPP~(mg~O[2]~L^-1~d^-1)), limits = c(0, 20), breaks = seq(0, 20, 5)) +
+   #
+   theme_classic() +
+   theme(panel.border = element_rect(fill=NA, color="black"),
+         legend.position = c(0.15, 0.88),
+         axis.ticks.length = unit(0.3, 'line'),
+         axis.text = element_text(color='black', size=rel(1)),
+         axis.text.x = element_text(hjust=0.2, margin = margin(t=0.5, unit='line')),
+         axis.title.x = element_text(margin = margin(t=0.5, unit="line"), size=rel(1.1)),
+         axis.title.y = element_text(margin = margin(r=0.5, unit="line"), size=rel(1.1)))
+
+# ggsave(file = "GPP.png")
+
+
+
 
 ## Bar Charts
 
