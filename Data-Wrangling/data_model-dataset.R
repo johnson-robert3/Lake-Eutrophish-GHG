5567ccc1050c83d9455d53f143282a0e68c92290
Robert Johnson <johnson.robert.333@gmail.com>
2023-06-22 15:59:42 -0500

    comment out data sets no longer used in analyses

--- Data-Wrangling/data_model-dataset.R
+++ Data-Wrangling/data_model-dataset.R
@@ -1,5 +1,5 @@
 
-# Large dataset of all variables/data for GAMs and LMEs
+# Large dataset of all variables/data for analyses
 
 
 # Needed data sets
@@ -10,13 +10,18 @@
 # sonde_surface  [all vars - temp, do, chla, phyco, cond, salinity]
 # sonde_bottom  [all vars - temp, do, chla, phyco, cond, salinity]
 # weather_data  [wind speed]
+# sonde_strat [profile z_mix; profile mixed/stratified status]
+
+
+# No longer needed/used
+
 # dea_rates  [DEA N2O]
 # methano_rates  [methanogenesis]
-# alk_data  [alkalinity]
+# ebullition
 # doc_data [DOC]
 # hobo_strat  [daily mean z_mix; daily mean z_mix between 900a-1130a; mean z_mix of previous 24h; percent of previous 24h mixed/stratified]
-# sonde_strat [profile z_mix; profile mixed/stratified status]
-# ebullition
+# alk_data  [alkalinity]
+
 
 
 ## Prep the individual data sets
@@ -28,7 +33,8 @@ m10 = lake_flux %>%
 
 # Daily metabolism estimates
 m11 = metabolism %>%
-   filter(!(GPP < 0 | R > 0))
+   # filter(!(GPP < 0 | R > 0)) %>%
+   filter(!(if_all(.cols = c(GPP, R, NEP), .fns = is.na)))
 
 
 # Surface water limno samples
@@ -60,74 +66,74 @@ m15 = weather_data %>%
 
 
 # DEA
-m16 = dea_rates %>%
-   select(pond_id, doy, n2o_rate) %>%
-   rename(DEA = n2o_rate)
+# m16 = dea_rates %>%
+#    select(pond_id, doy, n2o_rate) %>%
+#    rename(DEA = n2o_rate)
 
 
 # Methanogenesis
-m17 = methano_rates %>%
-   select(pond_id, doy, ch4_rate) %>%
-   rename(methanogenesis = ch4_rate)
+# m17 = methano_rates %>%
+#    select(pond_id, doy, ch4_rate) %>%
+#    rename(methanogenesis = ch4_rate)
 
 
 # Alkalinity
-m18 = alk_data %>%
-   select(-sample_id, -notes) %>%
-   relocate(pond_id) %>%
-   rename(alk_ph = ph)
+# m18 = alk_data %>%
+#    select(-sample_id, -notes) %>%
+#    relocate(pond_id) %>%
+#    rename(alk_ph = ph)
 
 
 # DOC
-m19 = doc_dat %>%
-   select(pond_id, doy, doc_ppb) %>%
-   arrange(pond_id, doy) %>%
-   # align dates of DOC samples with GHG samples
-   #  Pond A: DOC becomes offset from GHGs by one day starting at DOY 222
-   #  Pond B: DOC is offset from GHGs by one day for DOYs 223, 225, 227, 230, 232
-   #  Ponds C-F are all aligned correctly
-   mutate(doy = case_when(pond_id=='A' & doy > 221 ~ doy - 1,
-                          pond_id=='B' & doy %in% c(223, 225, 227, 230, 232) ~ doy - 1,
-                          TRUE ~ doy))
+# m19 = doc_dat %>%
+#    select(pond_id, doy, doc_ppb) %>%
+#    arrange(pond_id, doy) %>%
+#    # align dates of DOC samples with GHG samples
+#    #  Pond A: DOC becomes offset from GHGs by one day starting at DOY 222
+#    #  Pond B: DOC is offset from GHGs by one day for DOYs 223, 225, 227, 230, 232
+#    #  Ponds C-F are all aligned correctly
+#    mutate(doy = case_when(pond_id=='A' & doy > 221 ~ doy - 1,
+#                           pond_id=='B' & doy %in% c(223, 225, 227, 230, 232) ~ doy - 1,
+#                           TRUE ~ doy))
 
 
 # T-chain stratification
 
 # mean daily z-mix
-t1 = metab_data %>%
-   select(pond_id, doy, z_mix) %>%
-   group_by(pond_id, doy) %>%
-   summarize(zmix_daily = mean(z_mix)) %>%
-   ungroup()
+# t1 = metab_data %>%
+#    select(pond_id, doy, z_mix) %>%
+#    group_by(pond_id, doy) %>%
+#    summarize(zmix_daily = mean(z_mix)) %>%
+#    ungroup()
 
 # mean z-mix around time of GHG sample collection (9 - 1130 am)
-t2 = metab_data %>%
-   select(pond_id, date_time, doy, z_mix) %>%
-   mutate(hour = hour(date_time)) %>%
-   filter(hour %in% c(9:11)) %>%
-   group_by(pond_id, doy) %>%
-   summarize(zmix_ghg_time = mean(z_mix)) %>%
-   ungroup()
+# t2 = metab_data %>%
+#    select(pond_id, date_time, doy, z_mix) %>%
+#    mutate(hour = hour(date_time)) %>%
+#    filter(hour %in% c(9:11)) %>%
+#    group_by(pond_id, doy) %>%
+#    summarize(zmix_ghg_time = mean(z_mix)) %>%
+#    ungroup()
 
 # mean z-mix of previous 24 hours (10 - 10 am)
-t3 = metab_data %>%
-   select(pond_id, date_time, doy, z_mix) %>%
-   mutate(lagzmix = lag(z_mix, n=27)) %>% 
-   group_by(pond_id, doy) %>% 
-   summarize(zmix_prev24 = mean(lagzmix, na.rm=TRUE)) %>%
-   ungroup()
+# t3 = metab_data %>%
+#    select(pond_id, date_time, doy, z_mix) %>%
+#    mutate(lagzmix = lag(z_mix, n=27)) %>% 
+#    group_by(pond_id, doy) %>% 
+#    summarize(zmix_prev24 = mean(lagzmix, na.rm=TRUE)) %>%
+#    ungroup()
 
 # percent of measurements in previous 24 hours that were "stratified"
-t4 = metab_data %>%
-   mutate(stratification = case_when(z_mix==1.75 ~ 0,
-                                     z_mix==1.5 ~ 0,
-                                     TRUE ~ 1)) %>%
-   mutate(lagstrat = lag(stratification, n=27)) %>%
-   group_by(pond_id, doy) %>%
-   summarize(perc_strat_prev24 = mean(lagstrat, na.rm=TRUE) * 100) %>%
-   ungroup()
-
-m20 = full_join(t1, t2) %>% full_join(t3) %>% full_join(t4)
+# t4 = metab_data %>%
+#    mutate(stratification = case_when(z_mix==1.75 ~ 0,
+#                                      z_mix==1.5 ~ 0,
+#                                      TRUE ~ 1)) %>%
+#    mutate(lagstrat = lag(stratification, n=27)) %>%
+#    group_by(pond_id, doy) %>%
+#    summarize(perc_strat_prev24 = mean(lagstrat, na.rm=TRUE) * 100) %>%
+#    ungroup()
+# 
+# m20 = full_join(t1, t2) %>% full_join(t3) %>% full_join(t4)
 
 
 # Sonde profile stratification
@@ -136,8 +142,8 @@ m21 = sonde_strat %>%
 
 
 # Ebullition
-m22 = read_csv("Data/ebullition_total.csv") %>%
-   select(-week)
+# m22 = read_csv("Data/ebullition_total.csv") %>%
+#    select(-week)
 
 
 #- Combined
@@ -148,13 +154,13 @@ test = m10 %>%          # GHG dissolved conc. and diffusive flux
    full_join(m13) %>%   # sonde surface
    full_join(m14) %>%   # sonde bottom
    full_join(m15) %>%   # weather
-   full_join(m16) %>%   # DEA
-   full_join(m17) %>%   # Methanogenesis
-   full_join(m18) %>%   # alkalinity
-   full_join(m19) %>%   # DOC
-   full_join(m20) %>%   # t-chain stratification
-   full_join(m21) %>%   # sonde stratification
-   full_join(m22)       # Ebullition
+   # full_join(m16) %>%   # DEA
+   # full_join(m17) %>%   # Methanogenesis
+   # full_join(m18) %>%   # alkalinity
+   # full_join(m19) %>%   # DOC
+   # full_join(m20) %>%   # t-chain stratification
+   full_join(m21) #%>%   # sonde stratification
+   # full_join(m22)       # Ebullition
 
 
 test = test %>%
@@ -169,14 +175,17 @@ model_dataset = test %>%
    # reorder variables
    select(pond_id, treatment, doy, period, everything()) %>%
    arrange(pond_id, doy) %>%
+   
    # add N:P ratio variable
-   mutate(np_ratio = (tn/14.001) / ((tp/1000)/30.97),
-          # convert DOC to ppm (mg/L)
-          doc_ppm = doc_ppb / 1000) %>%
+   # mutate(np_ratio = (tn/14.001) / ((tp/1000)/30.97),
+   #        # convert DOC to ppm (mg/L)
+   #        doc_ppm = doc_ppb / 1000) %>%
+   
    # add a new 'period' variable to account for the fact that pulse periods don't mean anything for the reference treatment
    #  reference ponds are, in effect, always in the 'Base' period
-   mutate(period2 = if_else(treatment=="reference", "BASE", period)) %>%
-   relocate(period2, .after=period) %>%
+   # mutate(period2 = if_else(treatment=="reference", "BASE", period)) %>%
+   # relocate(period2, .after=period) %>%
+   
    # add a Date variable
    #  updated dates; as_date() treats the 'origin' as day 0, not day 1, so need to set it as the previous day to the desired start
    mutate(date = as_date(doy, origin="2019-12-31")) %>%
