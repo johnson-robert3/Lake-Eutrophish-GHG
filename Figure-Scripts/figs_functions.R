4fafee4106ab3883096da221d41d59188e16590f
Robert Johnson <johnson.robert.333@gmail.com>
2023-06-26 11:27:28 -0500

    remove code for old 2-panel (nutrient) and 3-panel (food web) figs

--- Figure-Scripts/figs_functions.R
+++ Figure-Scripts/figs_functions.R
@@ -4,33 +4,6 @@
 #~~~
 
 
-# 3-panel figures (by food web treatment)
-fig_aes_fw = function(.fig) {
-   
-   .fig +
-      
-      # Nutrient pulses
-      geom_vline(xintercept = c(176, 211), linetype=2, color="gray40") +
-      
-      ## Data as points
-      # geom_line(aes(group = trt_nutrients), size=0.5) +
-      # geom_point(color="white", fill="white", shape=21, size=3) +
-      # geom_point(aes(fill = trt_nutrients), shape=21, size=3, alpha=0.8) +
-      # scale_fill_manual(name = NULL,
-      #                   labels = c("no" = "Reference", "yes" = "Pulsed"),
-      #                   values = c("no" = "cornflowerblue", "yes" = "seagreen3")) +
-      
-      ## Data as lines
-      geom_line(aes(group = trt_nutrients, color = trt_nutrients), size=1.2, alpha=0.7) +
-      scale_color_manual(name = NULL,
-                         labels = c("no" = "Reference", "yes" = "Pulsed"),
-                         values = c("no" = "cornflowerblue", "yes" = "seagreen3")) +
-      
-      # Aesthetics
-      scale_x_continuous(name = "DOY", expand = expansion(mult=0.1)) +
-      theme_classic()
-   
-}
 
 
 # Buoyancy Frequency function for non-time-series data
