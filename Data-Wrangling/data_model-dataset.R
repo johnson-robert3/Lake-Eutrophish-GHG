32a6c90a35037aee776c6fc34fd4a57b01c4bd28
Robert Johnson <johnson.robert.333@gmail.com>
2024-07-26 10:29:01 -0500

    update to 'week' var in combined df; use EDI df for GHG flux; remove old unnecessary code

--- Data-Wrangling/data_model-dataset.R
+++ Data-Wrangling/data_model-dataset.R
@@ -2,7 +2,7 @@
 # Large dataset of all variables/data for analyses
 
 
-# Needed data sets
+#- Needed data sets
 
 # lake_flux  [co2, ch4, and n2o flux]
 # metabolism  [GPP, R, NEP]
@@ -11,24 +11,21 @@
 # sonde_bottom  [all vars - temp, do, chla, phyco, cond, salinity]
 # weather_data  [wind speed]
 # sonde_strat [profile z_mix; profile mixed/stratified status]
-# dea_rates  [DEA N2O]
-# methano_rates  [methanogenesis]
-# ebullition
-
-
-# No longer needed/used
-
-# doc_data [DOC]
-# hobo_strat  [daily mean z_mix; daily mean z_mix between 900a-1130a; mean z_mix of previous 24h; percent of previous 24h mixed/stratified]
-# alk_data  [alkalinity]
+# methano_dea [methanogenesis potential, DEA rates]
+# ebu_flux_pond
 
 
 
 ## Prep the individual data sets
 
 # Dissolved gas concentration and diffusive flux (from data set created and output in 'data_ghg-calculations' script)
-m10 = read_csv("Data/ghg_concentration_flux_total.csv") %>%
-   select(pond_id, doy, week, ends_with("flux"), ends_with("lake"))
+# m10 = read_csv("Data/ghg_concentration_flux_total.csv") %>%
+#    select(pond_id, doy, week, ends_with("flux"), ends_with("lake"))
+
+
+# Dissolved gas concentration and diffusive flux (from 'data_import_EDI' script)
+m10 = lake_flux %>%
+   select(pond_id, doy, ends_with("flux"), ends_with("lake"))
 
 
 # Daily metabolism estimates (from 'data_import_EDI' script)
@@ -65,80 +62,10 @@ m15 = weather_data %>%
    rename(wind_U10 = U10)
 
 
-# DEA
-# m16 = dea_rates %>%
-#    select(pond_id, doy, n2o_rate) %>%
-#    rename(DEA = n2o_rate)
-
-
-# Methanogenesis
-# m17 = methano_rates %>%
-#    select(pond_id, doy, ch4_rate) %>%
-#    rename(methanogenesis = ch4_rate)
-
-
 # Methanogenesis and DEA (from 'data_import_EDI' script)
 m16 = methano_dea %>%
-   rename(methanogenesis = ch4_rate, DEA = n2o_rate)
-
-
-# Alkalinity
-# m18 = alk_data %>%
-#    select(-sample_id, -notes) %>%
-#    relocate(pond_id) %>%
-#    rename(alk_ph = ph)
-
-
-# DOC
-# m19 = doc_dat %>%
-#    select(pond_id, doy, doc_ppb) %>%
-#    arrange(pond_id, doy) %>%
-#    # align dates of DOC samples with GHG samples
-#    #  Pond A: DOC becomes offset from GHGs by one day starting at DOY 222
-#    #  Pond B: DOC is offset from GHGs by one day for DOYs 223, 225, 227, 230, 232
-#    #  Ponds C-F are all aligned correctly
-#    mutate(doy = case_when(pond_id=='A' & doy > 221 ~ doy - 1,
-#                           pond_id=='B' & doy %in% c(223, 225, 227, 230, 232) ~ doy - 1,
-#                           TRUE ~ doy))
-
-
-# T-chain stratification
-
-# mean daily z-mix
-# t1 = metab_data %>%
-#    select(pond_id, doy, z_mix) %>%
-#    group_by(pond_id, doy) %>%
-#    summarize(zmix_daily = mean(z_mix)) %>%
-#    ungroup()
-
-# mean z-mix around time of GHG sample collection (9 - 1130 am)
-# t2 = metab_data %>%
-#    select(pond_id, date_time, doy, z_mix) %>%
-#    mutate(hour = hour(date_time)) %>%
-#    filter(hour %in% c(9:11)) %>%
-#    group_by(pond_id, doy) %>%
-#    summarize(zmix_ghg_time = mean(z_mix)) %>%
-#    ungroup()
-
-# mean z-mix of previous 24 hours (10 - 10 am)
-# t3 = metab_data %>%
-#    select(pond_id, date_time, doy, z_mix) %>%
-#    mutate(lagzmix = lag(z_mix, n=27)) %>% 
-#    group_by(pond_id, doy) %>% 
-#    summarize(zmix_prev24 = mean(lagzmix, na.rm=TRUE)) %>%
-#    ungroup()
-
-# percent of measurements in previous 24 hours that were "stratified"
-# t4 = metab_data %>%
-#    mutate(stratification = case_when(z_mix==1.75 ~ 0,
-#                                      z_mix==1.5 ~ 0,
-#                                      TRUE ~ 1)) %>%
-#    mutate(lagstrat = lag(stratification, n=27)) %>%
-#    group_by(pond_id, doy) %>%
-#    summarize(perc_strat_prev24 = mean(lagstrat, na.rm=TRUE) * 100) %>%
-#    ungroup()
-# 
-# m20 = full_join(t1, t2) %>% full_join(t3) %>% full_join(t4)
+   rename(methanogenesis = ch4_rate, DEA = n2o_rate) %>%
+   select(-week)
 
 
 # Sonde profile stratification (from 'data_import_EDI' script then 'data_stratification' script)
@@ -161,10 +88,6 @@ test = m10 %>%          # GHG dissolved conc. and diffusive flux
    full_join(m14) %>%   # sonde bottom
    full_join(m15) %>%   # weather
    full_join(m16) %>%   # DEA and Methanogenesis
-   # full_join(m17) %>%   # Methanogenesis
-   # full_join(m18) %>%   # alkalinity
-   # full_join(m19) %>%   # DOC
-   # full_join(m20) %>%   # t-chain stratification
    full_join(m21) %>%   # sonde stratification
    full_join(m22)       # Ebullition
 
@@ -175,25 +98,20 @@ test = test %>%
                                 pond_id %in% c("D", "E", "F") ~ "reference")) %>%
    relocate(treatment, .after=pond_id) %>%
    # remove 3 blank rows with extra wind speed data
-   filter(!(is.na(pond_id)))
+   filter(!(is.na(pond_id))) %>%
+   # filter to days of experiment
+   filter(doy %in% c(145:242)) %>%
+   # add a variable for "week" of experiment here (df's were joining weird when 'week' was kept in "m" df's above)
+   left_join(data.frame(doy = c(145:242), week = rep(1:14, each=7)))
 
 
 
 model_dataset = test %>%
    # reorder variables
-   select(pond_id, treatment, doy, period, everything()) %>%
-   arrange(pond_id, doy) %>%
-   
-   # add N:P ratio variable
-   # mutate(np_ratio = (tn/14.001) / ((tp/1000)/30.97),
-   #        # convert DOC to ppm (mg/L)
-   #        doc_ppm = doc_ppb / 1000) %>%
-   
-   # add a new 'period' variable to account for the fact that pulse periods don't mean anything for the reference treatment
-   #  reference ponds are, in effect, always in the 'Base' period
-   # mutate(period2 = if_else(treatment=="reference", "BASE", period)) %>%
-   # relocate(period2, .after=period) %>%
-   
+   select(pond_id, treatment, doy, week, period, everything()) %>%
+   arrange(pond_id, doy) %>% 
+   # fill in "period" variable for DOY 241
+   mutate(period = if_else(is.na(period) & doy==241, "PULSE2", period)) %>%
    # add a Date variable
    #  updated dates; as_date() treats the 'origin' as day 0, not day 1, so need to set it as the previous day to the desired start
    mutate(date = as_date(doy, origin="2019-12-31")) %>%
@@ -201,11 +119,11 @@ model_dataset = test %>%
 
 
 # output the complete model dataset
-write_csv(model_dataset, file = "Data/ghg-model-dataset_2023-08-07.csv")
+write_csv(model_dataset, file = "Data/ghg-model-dataset_2024-07-26.csv")
 
 
    ## remove temporary individual data sets
-   rm(list = ls(pattern = "m[[:digit:]]"), test, t1, t2, t3, t4)
+   rm(list = ls(pattern = "m[[:digit:]]"), test)
    ##
 
 
