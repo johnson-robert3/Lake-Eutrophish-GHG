e264bfe824f07d250b3333555e4296e1306af862
Robert Johnson <johnson.robert.333@gmail.com>
2025-06-26 15:57:51 -0500

    remove old, unused objects

--- Figure-Scripts/figs_functions.R
+++ Figure-Scripts/figs_functions.R
@@ -4,22 +4,13 @@
 #~~~
 
 
-# Commonly used values/vectors for figures
+## Commonly used values/vectors for figures
+
 
 # Nutrient treatment
-
 pulse_breaks = c("pulsed", "reference")
-# nut_breaks = c("yes", "no")
-
 pulse_color = c("pulsed" = "#5D3891", "reference" = "#F99417") # pulse = purple, ref = orange
-# nut_color = c("yes" = "#5D3891", "no" = "#F99417") # pulse-purple, ref-orange
-
 pulse_labs = c("pulsed" = "Pulsed", "reference" = "Reference")
-# nut_labs = c("yes" = "Pulsed", "no" = "Reference")
-
-# nut_color = c("yes" = "seagreen3", "no" = "cornflowerblue")
-# nut_color = c("yes" = "#e94560", "no" = "#0f3460") # pulse-red, ref-blue
-
 
 # Food web treatment
 fish_breaks = c("high", "medium", "low")
@@ -28,20 +19,4 @@ fish_labs = c("high" = "High", "medium" = "Intermediate", "low" = "Low")
 fish_color = c("high" = "#a8dda8", "medium" = "#51c2d5", "low" = "#0f3460")
 
 
-
-# # Buoyancy Frequency function for non-time-series data
-# profile_bf = function(.dat) {
-#    
-#    bf = buoyancy.freq(wtr = .dat$temp,
-#                       depths = .dat$depth_int)
-#    
-#    buoy_freq = as.vector(bf)
-#    
-#    depth = attr(bf, "depths")
-#    
-#    table = data.frame(depth, buoy_freq)
-#    
-#    return(table)
-#    
-# }
 
